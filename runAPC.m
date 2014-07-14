74402ec67a3ca1ca4bf6357d2da601811ffc6fad
edeno <edeno@users.noreply.github.com>
2014-07-14 11:55:47 -0400

    Update runAPC models

--- runAPC.m
+++ runAPC.m
@@ -1,15 +1,18 @@
 clear all; close all; clc;
 
-regressionModel_str = 'Rule * Switch History + Rule * Previous Error History + Rule * Test Stimulus + Normalized Prep Time';
+regressionModel_str = 'Rule * Switch History + Rule * Previous Error History + Rule * Congruency History + Response Direction + Normalized Prep Time';
 timePeriod = 'Rule Response';
 main_dir = '/data/home/edeno/Task Switching Analysis';
 numSim = 1000;
 numSamples = 50000;
 overwrite = true;
 
-computeAPC_rule(regressionModel_str, timePeriod, main_dir, 'numSim', numSim, ...
-    'numSamples', numSamples, 'overwrite', overwrite)
-computeAPC_rulepreverror(regressionModel_str, timePeriod, main_dir, 'numSim', numSim, ...
-    'numSamples', numSamples, 'overwrite', overwrite)
-computeAPC_ruleswitch(regressionModel_str, timePeriod, main_dir, 'numSim', numSim, ...
-    'numSamples', numSamples, 'overwrite', overwrite)
+% type = {'RuleCongruency2', 'RulePrevError2', 'RuleSwitch2', 'PrevError2', ...
+%     'Congruency2', 'Switch2', 'Rule2', 'ResponseDir2', 'RulePrevError2_low', 'RuleCongruency2_low'};
+
+type = {'Switch2'};
+
+for type_ind = 1:length(type),
+    computeAPC(regressionModel_str, timePeriod, main_dir, type{type_ind}, 'numSim', numSim, ...
+        'numSamples', numSamples, 'overwrite', overwrite)
+end
