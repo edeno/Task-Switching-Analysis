21f813d9f9f593a298f18ee302f7cf25fa0d7541
edeno <edeno@users.noreply.github.com>
2014-07-16 21:08:16 -0400

    Added rule ratio plot, reordered covariates
    Added code for plotting the ratio of rule-dependent changes in a given
    condition. Also reordered covariates so that Previous Error and No
    Previous Error are not alternating, adding to interpretability.

--- GAMfit/Analysis Scripts/coef_analysis.m
+++ GAMfit/Analysis Scripts/coef_analysis.m
@@ -45,9 +45,6 @@ numSim = size(neurons(1).par_sim, 1);
 numLevels = length(gam.level_names);
 numNeurons = length(neurons);
 
-par_sim = [neurons.par_sim];
-par_sim = reshape(par_sim, [numSim, numLevels, numNeurons]);
-
 % Create anonymouse functions that give the population mean and standard
 % deviation
 mean_pop = @(stat, filter_ind) mean(mean(stat(:, :, filter_ind), 3), 1);
@@ -56,20 +53,48 @@ ci_pop = @(stat, filter_ind) quantile(mean(stat(:, :, filter_ind), 3), [.025 .97
 std_pop = @(stat, filter_ind) mean(std(stat(:, :, filter_ind), [], 3), 1);
 std_ci_pop = @(stat, filter_ind) quantile(std(stat(:, :, filter_ind), [], 3), [.025 .975], 1);
 
-pct_change = @(x) (exp(x) - 1) * 100;
 mult_change = @(x) exp(x);
+pct_change = @(x) (x - 1) * 100;
 
-stat = {mult_change(par_sim), mult_change(abs(par_sim)), pct_change(par_sim), pct_change(abs(par_sim))};
-stat_names = {'Multiplicative Change', 'Abs. Mult. Change', 'Percent Change', 'Abs. Percent Change'};
-ref_line = [1, 1, 0, 0];
-xlims = {[.93 1.07], [.9 1.25], [-7 7], [-0.1 25]};
-
-%%
+%% Rule Ratios
 
 level_names = gam.level_names;
 cov_names = gam.cov_names;
+par_sim = [neurons.par_sim];
+par_sim = reshape(par_sim, [numSim, numLevels, numNeurons]);
+
+prevError_ind = ~cellfun(@isempty, regexp(level_names, 'Previous Error', 'match'));
+
+prevError_level_names = level_names(prevError_ind);
+level_names(prevError_ind) = [];
+level_names = [level_names prevError_level_names];
+
+prevError_cov_names = cov_names(prevError_ind);
+cov_names(prevError_ind) = [];
+cov_names = [cov_names prevError_cov_names];
+
+prevError_par_sim = par_sim(:, prevError_ind, :);
+par_sim(:, prevError_ind, :) = [];
+par_sim = cat(2, par_sim, prevError_par_sim);
 
 noPrevError_ind = ~cellfun(@isempty, regexp(level_names, 'No', 'match'));
+
+noPrevError_level_names = level_names(noPrevError_ind);
+level_names(noPrevError_ind) = [];
+level_names = [level_names noPrevError_level_names];
+
+noPrevError_cov_names = cov_names(noPrevError_ind);
+cov_names(noPrevError_ind) = [];
+cov_names = [cov_names noPrevError_cov_names];
+
+noPrevError_par_sim = par_sim(:, noPrevError_ind, :);
+par_sim(:, noPrevError_ind, :) = [];
+par_sim = cat(2, par_sim, noPrevError_par_sim);
+
+stat = {mult_change(par_sim), mult_change(abs(par_sim)), pct_change(mult_change(par_sim)), pct_change(mult_change(abs(par_sim)))};
+stat_names = {'Multiplicative Change', 'Abs. Mult. Change', 'Percent Change', 'Abs. Percent Change'};
+ref_line = [1, 1, 0, 0];
+xlims = {[.93 1.07], [.9 1.25], [-7 7], [-0.1 25]};
 
 cov_rule_ind = find(ismember(cov_names, 'Rule'));
 rule_names = level_names(cov_rule_ind);
@@ -79,15 +104,11 @@ numCovId = unique(cov_name_id);
 for stat_ind = 1:length(stat),
     
     figure;
-    ax = zeros(size(rule_names));
-    xLim = cell(size(rule_names));
-    xTick = cell(size(rule_names));
-    xTickLabel = cell(size(rule_names));
     
     for rule_ind = 1:length(rule_names),
         subplot(1,2,rule_ind);
         
-        levels_ind = find(~cellfun(@isempty, regexp(level_names, rule_names{rule_ind}, 'match')) & ~noPrevError_ind);
+        levels_ind = find(~cellfun(@isempty, regexp(level_names, rule_names{rule_ind}, 'match')));
         cur_numLevels = length(levels_ind);
         
         % dlPFC
@@ -109,7 +130,7 @@ for stat_ind = 1:length(stat),
             cur_connect_ind = levels_ind(ismember(cov_name_id(levels_ind), connect_ind));
             plot(pfc_data_mean(cur_connect_ind), find(ismember(cov_name_id(levels_ind), connect_ind)), 'b')
         end
-
+        
         % ACC
         acc_ind = ~pfc;
         
@@ -142,4 +163,100 @@ for stat_ind = 1:length(stat),
     end
 end
 
+%% Ratio of Rule Ratios
 
+level_names = gam.level_names;
+cov_names = gam.cov_names;
+par_sim = [neurons.par_sim];
+par_sim = reshape(par_sim, [numSim, numLevels, numNeurons]);
+
+noPrevError_ind = ~cellfun(@isempty, regexp(level_names, 'No', 'match'));
+
+noPrevError_level_names = level_names(noPrevError_ind);
+level_names(noPrevError_ind) = [];
+level_names = [level_names noPrevError_level_names];
+
+noPrevError_cov_names = cov_names(noPrevError_ind);
+cov_names(noPrevError_ind) = [];
+cov_names = [cov_names noPrevError_cov_names];
+
+noPrevError_par_sim = par_sim(:, noPrevError_ind, :);
+par_sim(:, noPrevError_ind, :) = [];
+par_sim = cat(2, par_sim, noPrevError_par_sim);
+
+nonInteraction_cov_names = cov_names(cellfun(@isempty, regexp(cov_names, ':', 'match')));
+nonInteraction_level_names = level_names(cellfun(@isempty, regexp(cov_names, ':', 'match')));
+unique_cov_names = unique(nonInteraction_cov_names(~ismember(nonInteraction_cov_names, {'(Intercept)', 'Rule', 'Response Direction'})));
+
+color_inter_name = @(x) strcat('Color:', x);
+orient_inter_name = @(x) strcat('Orientation:', x);
+
+par_sim_value = @(level_name) par_sim(:, ismember(level_names, level_name), :);
+mult_ratio = @(level1, level2) mult_change(par_sim_value(level1))./mult_change(par_sim_value(level2));
+abs_mult_ratio = @(level1, level2) max(mult_ratio(level1, level2), mult_ratio(level2, level1));
+
+f(1) = figure;
+f(2) = figure;
+
+plot_ind = numSubplots(length(unique_cov_names));
+
+for cov_ind = 1:length(unique_cov_names),
+    
+    cur_cov_level_names = nonInteraction_level_names(ismember(nonInteraction_cov_names, unique_cov_names(cov_ind)));
+    
+    cur_color_inter_names = color_inter_name(cur_cov_level_names);
+    cur_orient_inter_names = orient_inter_name(cur_cov_level_names);
+    
+    stat = {mult_ratio(cur_orient_inter_names, cur_color_inter_names), abs_mult_ratio(cur_orient_inter_names, cur_color_inter_names)};
+    stat_names = {'Mult. Ratio', 'Abs. Mult. Ratio'};
+    
+    cur_numLevels = length(cur_cov_level_names);   
+    
+    for stat_ind = 1:length(stat),
+        
+        figure(f(stat_ind));
+        subplot(plot_ind(1), plot_ind(2), cov_ind);
+        % dlPFC
+        pfc_ind = pfc;
+        
+        pfc_data_mean = mean_pop(stat{stat_ind}, pfc_ind)';
+        pfc_data_ci = ci_pop(stat{stat_ind}, pfc_ind)';
+        
+        pfc_data_ci = convert_bounds(pfc_data_mean, pfc_data_ci);
+        
+        h1 = herrorbar(pfc_data_mean, 1:cur_numLevels, pfc_data_ci(:,1), pfc_data_ci(:,2), 'bo');
+        set(h1, 'MarkerSize', 6, ...
+            'LineWidth', 3 ...
+            );
+        
+        hold all;
+        
+        plot(pfc_data_mean, 1:cur_numLevels, 'b')
+        
+        % ACC
+        acc_ind = ~pfc;
+        
+        acc_data_mean = mean_pop(stat{stat_ind}, acc_ind)';
+        acc_data_ci = ci_pop(stat{stat_ind}, acc_ind)';
+        
+        acc_data_ci = convert_bounds(acc_data_mean, acc_data_ci);
+        
+        h2 = herrorbar(acc_data_mean, 1:cur_numLevels, acc_data_ci(:,1), acc_data_ci(:,2), 'go');
+        set(h2, 'MarkerSize', 6, ...
+            'LineWidth', 3 ...
+            );
+        
+        plot(acc_data_mean, 1:cur_numLevels, 'g')
+        
+        box off;
+        set(gca, 'XAxisLocation', 'top')
+        set(gca, 'YTick', 1:cur_numLevels);
+        set(gca, 'YTickLabel', cur_cov_level_names);
+        xlabel([stat_names{stat_ind}, ' of Rules']);
+        ylim([0.5 cur_numLevels+0.5]);
+        vline(1, 'r:', 'Baseline');
+        
+    end
+    
+    
+end
