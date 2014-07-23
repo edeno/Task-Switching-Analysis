7e9bd01f6d089c044ab858e9ac78f64787f26366
edeno <edeno@users.noreply.github.com>
2014-07-23 19:24:49 -0400

    Updated resubmit to work with 2013b

--- Helper Functions/resumbitError_cluster.m
+++ Helper Functions/resumbitError_cluster.m
@@ -1,12 +1,20 @@
-% Note: does not work with current version of matlab on cluster R2013b
-% need matlab 2014a or greater
 jobMan = parcluster();
-failedJobs = findJob(jobMan,'Username','edeno','State','failed');
-numJobs = length(failedJobs);
-newJob = cell(1, numJobs);
+finishedJobs = findJob(jobMan, 'State', 'finished', 'Username', 'edeno');
 
-for job_ind = 1:numJobs,
-    newJob{job_ind} = recreate(failedJobs(job_ind));
+errorTasks = [finishedJobs.Tasks];
+error_message = {errorTasks(1, :).ErrorMessage};
+isError = ~strcmp(error_message, '');
+
+errorJobs = finishedJobs(isError);
+
+numNewJobs = length(errorJobs);
+newJob = cell(1, numNewJobs);
+
+for job_ind = 1:numNewJobs,
+    fprintf('\n Resubmitting Job ID: %d \n', errorJobs(job_ind).ID);
+    newJob{job_ind} = createCommunicatingJob(jobMan, 'AdditionalPaths', errorJobs(job_ind).AdditionalPaths, 'AttachedFiles', ...
+        errorJobs(job_ind).AttachedFiles, 'NumWorkersRange', errorJobs(job_ind).NumWorkersRange, 'Type', errorJobs(job_ind).Type);
+    
+    createTask(newJob{job_ind}, errorJobs(job_ind).Tasks(1).Function, errorJobs(job_ind).Tasks(1).NumOutputArguments, errorJobs(job_ind).Tasks(1).InputArguments);
     submit(newJob{job_ind});
 end
-
