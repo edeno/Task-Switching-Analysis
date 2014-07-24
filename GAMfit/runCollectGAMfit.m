3770c15a7660e97d29eb6338125464e9133fe370
Eric Denovellis <edeno@Erics-MacBook-Air.local>
2014-07-23 21:59:17 -0400

    Updated collectGAM to work for all time periods

--- GAMfit/runCollectGAMfit.m
+++ GAMfit/runCollectGAMfit.m
@@ -1,16 +1,19 @@
+%% Collects GAMfit session files into one file
 clear all; close all; clc;
 main_dir = '/data/home/edeno/Task Switching Analysis';
-load(sprintf('%s/paramSet.mat', main_dir), 'data_info');
-timePeriod = 'Rule Response';
+load(sprintf('%s/paramSet.mat', main_dir), 'data_info', 'validFolders');
 
-models_dir = sprintf('%s/%s/Models', data_info.processed_dir, timePeriod);
-
-models = dir(models_dir);
-models = {models.name};
-models = models(~ismember(models, {'.', '..'}));
-
-for models_ind = 1:length(models),
-    fprintf('\nModel: %s\n', models{models_ind});
-    collectGAMfit(models{models_ind}, timePeriod);
+for time_ind = 1:length(validFolders),
     
+    models_dir = sprintf('%s/%s/Models', data_info.processed_dir, validFolders{time_ind});
+    
+    models = dir(models_dir);
+    models = {models.name};
+    models = models(~ismember(models, {'.', '..'}));
+    
+    for models_ind = 1:length(models),
+        fprintf('\nModel: %s\n', models{models_ind});
+        collectGAMfit(models{models_ind}, timePeriod);
+        
+    end
 end
