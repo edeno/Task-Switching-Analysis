cf9016cf53f004692f215a01b7ff9996052944aa
Eric Denovellis <edeno@Erics-MacBook-Air.local>
2014-07-23 22:30:43 -0400

    Minor Fixes to collectGAMfit

--- GAMfit/runCollectGAMfit.m
+++ GAMfit/runCollectGAMfit.m
@@ -4,7 +4,7 @@ main_dir = '/data/home/edeno/Task Switching Analysis';
 load(sprintf('%s/paramSet.mat', main_dir), 'data_info', 'validFolders');
 
 for time_ind = 1:length(validFolders),
-    
+    fprintf('\nTime Period: %s\n', validFolders{time_ind});
     models_dir = sprintf('%s/%s/Models', data_info.processed_dir, validFolders{time_ind});
     
     models = dir(models_dir);
@@ -12,8 +12,7 @@ for time_ind = 1:length(validFolders),
     models = models(~ismember(models, {'.', '..'}));
     
     for models_ind = 1:length(models),
-        fprintf('\nModel: %s\n', models{models_ind});
-        collectGAMfit(models{models_ind}, timePeriod);
-        
+        fprintf('\tModel: %s\n', models{models_ind});
+        collectGAMfit(models{models_ind}, validFolders{time_ind});
     end
 end
