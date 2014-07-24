10122f2ec2d9d469689f1b8f05374b4919749b17
Eric Denovellis <edeno@Erics-MacBook-Air.local>
2014-07-23 21:54:01 -0400

    Made PCA analysis more general

--- GAMfit/Analysis Scripts/pca_coef_analysis.m
+++ GAMfit/Analysis Scripts/pca_coef_analysis.m
@@ -1,45 +1,96 @@
 clear all; close all; clc;
-cd('C:\Users\edeno\Dropbox\GAM Analysis\Rule Response');
+drop_path = getappdata(0, 'drop_path');
+cd([drop_path, '/GAM Analysis/Rule Response']);
 load('neurons.mat');
 
+%% Latent structure of the parameters
 pfc = logical([neurons.pfc]);
 par_est = [neurons.par_est]';
-baseline_firing = exp(par_est(1, :))*1000;
-par_est = par_est(:, 2:end);
+baseline_firing = exp(par_est(:,1))*1000;
+good_ind = baseline_firing > 0.5;
+par_est = par_est(good_ind, 2:end);
+pfc = pfc(good_ind);
 
 level_names = gam.level_names;
 level_names = level_names(2:end);
-%%
 
-[coefs_pfc,score_pfc,latent_pfc] = princomp(par_est(pfc, :));
-[coefs_acc,score_acc,latent_acc] = princomp(par_est(~pfc, :));
 
+numComponents = 3;
+plot_inds = numSubplots(numComponents);
+area_name = {'ACC', 'dlPFC'};
+
+for area_ind = 0:1,
+    
+    % Compute PCA
+    [coefs,score,latent, ~, explained] = pca(par_est(pfc == area_ind, :),'NumComponents', 10);
+    
+    figure;
+    plot_counter = 1;
+    % Plot component bi-plots
+    for comp1 = 1:numComponents,
+        for comp2 = comp1+1:numComponents,
+            
+            subplot(plot_inds(1),plot_inds(2),plot_counter);
+            biplot(coefs(:,[comp1, comp2]), 'scores',score(:,[comp1, comp2]), 'varlabels',level_names);
+            set(findobj(gcf,'Type','text'),'FontSize',7)
+            box off;
+            xlabel(sprintf('Component %d', comp1));
+            ylabel(sprintf('Component %d', comp2));
+            plot_counter = plot_counter + 1;
+        end
+    end
+    
+    suptitle(area_name{area_ind+1});
+    
+    figure;
+    pareto(explained);
+    xlabel('Principal Component');
+    ylabel('Variance Explained (%)');
+    title(area_name{area_ind+1});
+    box off;
+end
+
+%% Latent Structure of the neurons
+clear all;
+drop_path = getappdata(0, 'drop_path');
+cd([drop_path, '/GAM Analysis/Rule Response']);
+load('neurons.mat');
+
+pfc = logical([neurons.pfc]);
+par_est = [neurons.par_est];
+baseline_firing = exp(par_est(1,:))*1000;
+good_ind = baseline_firing > 0.5;
+par_est = par_est(2:end, good_ind);
+pfc = pfc(good_ind);
+
+numComponents = 3;
+plot_inds = numSubplots(numComponents);
+area_name = {'ACC', 'dlPFC'};
+
+% Compute PCA
+[coefs,score,latent, ~, explained] = pca(par_est, 'NumComponents', 10);
 
 figure;
-subplot(2,2,1);
+plot_counter = 1;
+% Plot component bi-plots
+for comp1 = 1:numComponents,
+    for comp2 = comp1+1:numComponents,
+        
+        subplot(plot_inds(1),plot_inds(2),plot_counter);
+        biplot(coefs(:,[comp1, comp2]), 'scores',score(:,[comp1, comp2]));
+        
+        
+        set(findobj(gcf,'Type','text'),'FontSize',7)
+        box off;
+        xlabel(sprintf('Component %d', comp1));
+        ylabel(sprintf('Component %d', comp2));
+        plot_counter = plot_counter + 1;
+    end
+end
 
-biplot(coefs_pfc(:,1:2), 'scores',score_pfc(:,1:2), 'varlabels',level_names);
-set(findobj(gcf,'Type','text'),'FontSize',7) 
-title('dlPFC');
-box off;
-
-subplot(2,2,2);
-
-biplot(coefs_acc(:,1:2), 'scores',score_acc(:,1:2), 'varlabels',level_names);
-set(findobj(gcf,'Type','text'),'FontSize',7) 
-title('ACC');
-box off;
-
-subplot(2,2,3);
-
-biplot(coefs_pfc(:,2:3), 'scores',score_pfc(:,2:3), 'varlabels',level_names);
-set(findobj(gcf,'Type','text'),'FontSize',7) 
-title('dlPFC');
-box off;
-
-subplot(2,2,4);
-
-biplot(coefs_acc(:,2:3), 'scores',score_acc(:,2:3), 'varlabels',level_names);
-set(findobj(gcf,'Type','text'),'FontSize',7) 
-title('ACC');
+figure;
+pareto(explained);
+xlabel('Principal Component');
+ylabel('Variance Explained (%)');
+title(area_name{area_ind+1});
 box off;
