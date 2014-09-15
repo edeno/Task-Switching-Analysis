f02a5d9e8759d23ab0fe7b5e8b7834852d4cba25
edeno <edeno@users.noreply.github.com>
2014-09-15 12:20:25 -0400

    Generalized function to work for all time periods

--- Average Predictive Comparison/collectAPC.m
+++ Average Predictive Comparison/collectAPC.m
@@ -1,15 +1,34 @@
+% Aggregate Average Predictive Comparisons into one file per comparison
 clear all; close all; clc;
-
-regressionModel_str = 'Rule * Switch History + Rule * Previous Error History + Rule * Congruency History + Response Direction + Rule * Normalized Prep Time';
-timePeriod = 'Rule Response';
 main_dir = '/data/home/edeno/Task Switching Analysis';
-apc_dir = [main_dir, '/Processed Data/', timePeriod,'/Models/', regressionModel_str, '/APC/'];
-
-covariate_type = dir(apc_dir);
-covariate_type = {covariate_type.name};
-covariate_type(ismember(covariate_type, {'.', '..'})) = [];
 overwrite = true;
+load([main_dir, '/paramSet.mat'], 'validFolders');
 
-for cov_ind = 1:length(covariate_type),
-    collectAPCs(regressionModel_str, timePeriod, main_dir, covariate_type{cov_ind}, 'overwrite', overwrite)
+timePeriod = validFolders;
+numTimePeriods = length(validFolders);
+
+% Loop over different time periods in the trial
+for time_ind = 1:numTimePeriods,
+    
+    models_dir = [main_dir, '/Processed Data/', timePeriod{time_ind},'/Models/'];
+    model = dir(models_dir);
+    model = {model.name};
+    model(ismember(model, {'.', '..'})) = [];
+    numModels = length(model);
+    
+    % Loop over fitted models
+    for model_ind = 1:numModels,
+        
+        apc_dir = [models_dir, model{model_ind}, '/APC/'];
+        
+        covariate_type = dir(apc_dir);
+        covariate_type = {covariate_type.name};
+        covariate_type(ismember(covariate_type, {'.', '..'})) = [];
+        
+        % Loop over the average predictive comparison fits and collect them
+        % into one file
+        for cov_ind = 1:length(covariate_type),
+            collectAPCs(model{model_ind}, timePeriod{time_ind}, main_dir, covariate_type{cov_ind}, 'overwrite', overwrite)
+        end
+    end
 end
