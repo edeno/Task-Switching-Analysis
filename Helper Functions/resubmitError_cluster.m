311351135f37992aebfdda837aad03dc0f66a436
edeno <edeno@users.noreply.github.com>
2014-10-15 15:33:36 -0400

    Generalized function to independent jobs

--- Helper Functions/resubmitError_cluster.m
+++ Helper Functions/resubmitError_cluster.m
@@ -1,20 +1,40 @@
 jobMan = parcluster();
 finishedJobs = findJob(jobMan, 'State', 'finished', 'Username', 'edeno');
 
-errorTasks = [finishedJobs.Tasks];
-error_message = {errorTasks(1, :).ErrorMessage};
-isError = ~strcmp(error_message, '');
-
-errorJobs = finishedJobs(isError);
-
-numNewJobs = length(errorJobs);
-newJob = cell(1, numNewJobs);
-
-for job_ind = 1:numNewJobs,
-    fprintf('\n Resubmitting Job ID: %d \n', errorJobs(job_ind).ID);
-    newJob{job_ind} = createCommunicatingJob(jobMan, 'AdditionalPaths', errorJobs(job_ind).AdditionalPaths, 'AttachedFiles', ...
-        errorJobs(job_ind).AttachedFiles, 'NumWorkersRange', errorJobs(job_ind).NumWorkersRange, 'Type', errorJobs(job_ind).Type);
+for job_ind = 1:length(finishedJobs),
     
-    createTask(newJob{job_ind}, errorJobs(job_ind).Tasks(1).Function, errorJobs(job_ind).Tasks(1).NumOutputArguments, errorJobs(job_ind).Tasks(1).InputArguments);
-    submit(newJob{job_ind});
+    errorTasks = [finishedJobs(job_ind).Tasks];
+    error_message = {errorTasks.ErrorMessage};
+    isError = ~strcmp(error_message, '');
+    if any(isError),
+        
+        errorJobs = finishedJobs(job_ind);
+        
+        fprintf('\n Resubmitting Job ID: %d \n', errorJobs.ID);
+        if ~strcmp(errorJobs.Type, 'independent'),
+            newJob = createCommunicatingJob(jobMan, 'AdditionalPaths', errorJobs.AdditionalPaths, 'AttachedFiles', ...
+                errorJobs.AttachedFiles, 'NumWorkersRange', errorJobs.NumWorkersRange, 'Type', errorJobs.Type);
+            task_id = find(isError);
+            for task_ind = 1:length(task_id),
+                createTask(newJob, ...
+                    errorJobs.Tasks(task_id(task_ind)).Function, ...
+                    errorJobs.Tasks(task_id(task_ind)).NumOutputArguments, ...
+                    errorJobs.Tasks(task_id(task_ind)).InputArguments);
+            end
+            submit(newJob);
+        else
+            newJob = createJob(jobMan, 'AdditionalPaths', errorJobs.AdditionalPaths, 'AttachedFiles', ...
+                errorJobs.AttachedFiles, 'NumWorkersRange', errorJobs.NumWorkersRange);
+            task_id = find(isError);
+            for task_ind = 1:length(task_id),
+                createTask(newJob, ...
+                    errorJobs.Tasks(task_id(task_ind)).Function, ...
+                    errorJobs.Tasks(task_id(task_ind)).NumOutputArguments, ...
+                    errorJobs.Tasks(task_id(task_ind)).InputArguments);
+            end
+            submit(newJob);
+        end
+    else
+        continue;
+    end
 end
