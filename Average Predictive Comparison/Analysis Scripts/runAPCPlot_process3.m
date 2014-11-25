e2e2e347750010fbd6a05ad05c9031b2b843f5ac
Eric Denovellis <edeno@Erics-MacBook-Air.local>
2014-11-25 00:25:08 -0500

    Normalized apc

--- Average Predictive Comparison/Analysis Scripts/runAPCPlot_process3.m
+++ Average Predictive Comparison/Analysis Scripts/runAPCPlot_process3.m
@@ -1,5 +1,5 @@
 clear all; close all; clc;
-apc_type = 'apc';
+apc_type = 'norm_apc';
 
 valid_models = {'Rule * Switch History + Rule * Previous Error + Rule * Previous Congruency', ...
     'Rule * Switch History + Rule * Previous Error + Rule * Congruency History + Previous Error * Response Direction + Rule * Normalized Prep Time'};
