3c52895648a894f8baeb2687083fab3b8ef3e0d8
edeno <edeno@users.noreply.github.com>
2015-01-12 15:14:04 -0500

    Handle fixation breaks

--- Helper Functions/convertFile_toJSON.m
+++ Helper Functions/convertFile_toJSON.m
@@ -6,7 +6,6 @@ cd(data_dir);
 save_dir = [drop_dir, '/Visualizations/Raster'];
 load([data_dir, '/', cur_file, '_GLMCov.mat']);
 load('behavior.mat');
-clear incorrect;
 
 behavior = behavior(ismember({behavior.session_name}, cur_file));
 isAttempted = behavior.attempted;
@@ -33,46 +32,33 @@ congruencyHistory_levels = GLMCov(ismember({GLMCov.name}, 'Congruency History'))
 testStimulus_levels = GLMCov(ismember({GLMCov.name}, 'Test Stimulus')).levels;
 ruleCue_levels = GLMCov(ismember({GLMCov.name}, 'Rule Cues')).levels;
 ruleCueRepetition_levels = GLMCov(ismember({GLMCov.name}, 'Rule Cue Switch')).levels;
-incorrect_levels = {'Correct', 'Incorrect'};
+isCorrect_levels = {'Incorrect', 'Correct', };
+isAttempted_levels = {'Not Attempted', 'Attempted'};
+fixationBreak_levels = {'No Fixation Break', 'Fixation Break'};
 
 behavior.Previous_Error(isnan(behavior.Previous_Error)) = 1;
 behavior.Congruency_History(isnan(behavior.Congruency_History)) = 1;
 
-Rule = rule_levels(behavior.Rule);
-Preparation_Time = behavior.Prep_Time';
-Response_Direction = responseDir_levels(behavior.Response_Direction);
-Previous_Error = previousError_levels(behavior.Previous_Error);
+Rule = rule_levels(behavior.Rule)';
+Preparation_Time = behavior.Prep_Time;
+Response_Direction = responseDir_levels(behavior.Response_Direction)';
+Previous_Error = previousError_levels(behavior.Previous_Error)';
 Congruency_History = congruencyHistory_levels(behavior.Congruency_History);
 Current_Congruency = Congruency_History(:,1);
 Previous_Congruency = Congruency_History(:,2);
-Test_Stimulus = testStimulus_levels(behavior.Test_Stimulus);
-Rule_Cues = ruleCue_levels(behavior.Rule_Cues);
-Rule_Cue_Repetition = ruleCueRepetition_levels(behavior.Rule_Cue_Switch);
-Incorrect = incorrect_levels(behavior.incorrect + 1);
+Test_Stimulus = testStimulus_levels(behavior.Test_Stimulus)';
+Rule_Cues = ruleCue_levels(behavior.Rule_Cues)';
+Rule_Cue_Repetition = ruleCueRepetition_levels(behavior.Rule_Cue_Switch)';
+Rule_Repetition = behavior.Switch_History;
+isCorrect = isCorrect_levels(behavior.correct + 1)';
+isAttempted = isAttempted_levels(isAttempted + 1)';
+fixationBreak = fixationBreak_levels(behavior.fixationBreak + 1)';
 
 fixOn_time = behavior.ITI_Time;
 ruleOn_time = fixOn_time + behavior.fixOn_time + behavior.Fix_Time; % Fix spot on + fix spot accquired + fixation time
 stimOn_time = ruleOn_time + behavior.Prep_Time;
 react_time = stimOn_time + behavior.Reaction_Time;
 reward_time = react_time + behavior.Saccade_Time;
-
-Rule_Repetition = behavior.Switch_History(isAttempted);
-Rule = Rule(isAttempted)';
-Response_Direction = Response_Direction(isAttempted)';
-Preparation_Time = Preparation_Time(isAttempted)';
-Previous_Error = Previous_Error(isAttempted)';
-Current_Congruency = Current_Congruency(isAttempted);
-Previous_Congruency = Previous_Congruency(isAttempted);
-Test_Stimulus = Test_Stimulus(isAttempted)';
-Rule_Cues = Rule_Cues(isAttempted)';
-Rule_Cue_Repetition = Rule_Cue_Repetition(isAttempted)';
-Incorrect = Incorrect(isAttempted)';
-
-fixOn_time = fixOn_time(isAttempted);
-ruleOn_time = ruleOn_time(isAttempted);
-stimOn_time = stimOn_time(isAttempted);
-react_time = react_time(isAttempted);
-reward_time = reward_time(isAttempted);
 
 for trial_ind = 1:numTrials,
     
@@ -97,14 +83,20 @@ for trial_ind = 1:numTrials,
     trials(trial_ind).Test_Stimulus = Test_Stimulus{trial_num(trial_ind)};
     trials(trial_ind).Rule_Cues = Rule_Cues{trial_num(trial_ind)};
     trials(trial_ind).Rule_Cue_Repetition = Rule_Cue_Repetition{trial_num(trial_ind)};
-    trials(trial_ind).Incorrect = Incorrect{trial_num(trial_ind)};
+    trials(trial_ind).isCorrect = isCorrect{trial_num(trial_ind)};
+    trials(trial_ind).isAttempted = isAttempted{trial_num(trial_ind)};
+    trials(trial_ind).Fixation_Break = fixationBreak{trial_num(trial_ind)};
     
     for neuron_ind = 1:numNeurons,
         cur_spikes = spikes(cur_trial, neuron_ind);
         cur_spikes(isnan(cur_spikes)) = 0;
         cur_time = trial_time(cur_trial);
         neuron_name = sprintf('%s_%d_%d', cur_file, wire_number(neuron_ind), unit_number(neuron_ind));
-        trials(trial_ind).(neuron_name) = cur_time(logical(cur_spikes));    
+        if ~behavior.fixationBreak(trial_ind),
+            trials(trial_ind).(neuron_name) = cur_time(logical(cur_spikes));
+        else
+            trials(trial_ind).(neuron_name) = NaN;
+        end
     end
     
 end
@@ -112,8 +104,9 @@ end
 data2json.neurons = neurons;
 data2json.trials = trials;
 
-cur_json = savejson(cur_file, data2json);
+opt.FileName = [cur_file, '.json'];
+opt.NaN = 'null';
+
 cd(save_dir);
-fid = fopen([cur_file, '.json'], 'w');
-fprintf(fid, cur_json);
-fclose(fid);
+cur_json = savejson(cur_file, data2json, opt);
+
