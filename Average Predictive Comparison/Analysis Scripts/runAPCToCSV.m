51b53bffcc39d2d1e6de973105f2d9e0a1006933
Eric Denovellis <edeno@Erics-MacBook-Air.local>
2015-02-25 11:00:20 -0500

    Update models

--- Average Predictive Comparison/Analysis Scripts/runAPCToCSV.m
+++ Average Predictive Comparison/Analysis Scripts/runAPCToCSV.m
@@ -1,7 +1,7 @@
 apc_type = 'norm_apc';
 
-valid_models = {'Rule * Rule Repetition + Rule * Previous Error + Rule * Previous Congruency + Session Time', ...
-    'Rule * Rule Repetition + Rule * Previous Error + Rule * Congruency History + Previous Error * Response Direction + Rule * Indicator Prep Time + Session Time'};
+valid_models = {'Rule * Rule Repetition + Rule * Previous Error History + Rule * Previous Congruency + Session Time', ...
+    'Rule * Rule Repetition + Rule * Previous Error History + Rule * Congruency History + Previous Error History * Response Direction + Rule * Indicator Prep Time + Session Time'};
 
 apcToCSV(apc_type, valid_models);
 apcToCSV_RuleBy(apc_type, valid_models);
