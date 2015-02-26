edc0e7d6f9e8ed91652eee5a3548cfbf2fcbaca5
edeno <edeno@users.noreply.github.com>
2015-02-26 10:34:23 -0500

    Update models

--- Average Predictive Comparison/Analysis Scripts/runAPCToCSV.m
+++ Average Predictive Comparison/Analysis Scripts/runAPCToCSV.m
@@ -1,7 +1,7 @@
 apc_type = 'norm_apc';
 
-valid_models = {'Rule * Rule Repetition + Rule * Previous Error History + Rule * Previous Congruency + Session Time', ...
-    'Rule * Rule Repetition + Rule * Previous Error History + Rule * Congruency History + Previous Error History * Response Direction + Rule * Indicator Prep Time + Session Time'};
+valid_models = {'Rule * Rule Repetition + Rule * Previous Error History Indicator + Rule * Previous Congruency + Session Time', ...
+    'Rule * Rule Repetition + Rule * Previous Error History Indicator + Rule * Congruency History + Previous Error History Indicator * Response Direction + Rule * Indicator Prep Time + Session Time'};
 
 apcToCSV(apc_type, valid_models);
 apcToCSV_RuleBy(apc_type, valid_models);
