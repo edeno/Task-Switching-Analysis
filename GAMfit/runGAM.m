5f48801afc444641bdfcdc9e39152c096828f469
edeno <edeno@users.noreply.github.com>
2015-03-26 14:47:00 -0400

    Chang regularization parameters for billionth time

--- GAMfit/runGAM.m
+++ GAMfit/runGAM.m
@@ -1,32 +1,15 @@
 clear all; close all; clc;
 main_dir = '/data/home/edeno/Task Switching Analysis';
-ridgeLambda = 10;
-numFolds = 1;
+ridgeLambda = 10^(-1:3);
+numFolds = 5;
 isOverwrite = true;
 
 %% Pre-Test Stimulus
 timePeriods = {'Intertrial Interval', 'Fixation', 'Rule Stimulus'};
 
 % All Interactions
-model{1} = 'Rule * Rule Repetition + Rule * Previous Error + Rule * Previous Congruency + Session Time';
-% % All Interactions w/o Congruency
-% model{2} = 'Rule * Rule Repetition + Rule * Previous Error + Session Time';
-% % All Interactions w/o Congruency, Previous Error
-% model{3} = 'Rule * Rule Repetition + Session Time';
-% % All Interactions w/o Congruency, Repetitions
-% model{4} = 'Rule * Previous Error + Session Time';
-% % No Interactions w/o Congruency
-% model{5} = 'Rule + Rule Repetition + Previous Error + Session Time';
-% % No Interactions w/o Congruency, Repetitions
-% model{6} = 'Rule + Previous Error + Session Time';
-% % No Interactions w/o Congruency, Previous Error History
-% model{7} = 'Rule + Rule Repetition + Session Time';
-% % No Interactions w/o Congruency, Rule
-% model{8} = 'Rule Repetition + Previous Error + Session Time';
-% % No Interactions w/o Congruency, Rule, Rule Repetition
-% model{8} = 'Previous Error + Session Time';
-% % No Interactions w/o Congruency, Rule Repetition, Previous Error History
-% model{9} = 'Rule + Session Time';
+model{1} = 'Rule + Rule Repetition + Previous Error + Previous Congruency';
+model{2} = 'Rule * Rule Repetition + Rule * Previous Error + Rule * Previous Congruency';
 
 for time_ind = 1:length(timePeriods),
     fprintf('\n\t Time Period: %s\n', timePeriods{time_ind});
@@ -38,7 +21,8 @@ end
 
 %% Post-Test Stimulus
 timePeriods = {'Stimulus Response', 'Saccade', 'Reward'};
-model{1} = 'Rule * Rule Repetition + Rule * Previous Error + Rule * Congruency History + Previous Error * Response Direction + Rule * Indicator Prep Time + Session Time';
+model{1} = 'Rule + Rule Repetition + Congruency History + Previous Error * Response Direction + Indicator Prep Time';
+model{2} = 'Rule * Rule Repetition + Rule * Previous Error + Rule * Congruency History + Previous Error * Response Direction + Rule * Indicator Prep Time';
 
 for time_ind = 1:length(timePeriods),
     fprintf('\n\t Time Period: %s\n', timePeriods{time_ind});
@@ -47,3 +31,16 @@ for time_ind = 1:length(timePeriods),
         GAMcluster(model{model_ind}, timePeriods{time_ind}, main_dir, 'numFolds', numFolds, 'overwrite', isOverwrite, 'ridgeLambda', ridgeLambda);        
     end
 end
+
+%% Post-Test Stimulus - over time
+% timePeriods = {'Stimulus Response'};
+% model{1} = 's(Rule, Trial Time) + s(Rule Repetition, Trial Time) + s(Previous Error, Trial Time) + s(Congruency History, Trial Time) + s(Response Direction, Trial Time) + s(Indicator Prep Time, Trial Time)';
+% model{2} = 'Rule * Rule Repetition + Rule * Previous Error + Rule * Congruency History + Previous Error * Response Direction + Rule * Indicator Prep Time';
+% 
+% for time_ind = 1:length(timePeriods),
+%     fprintf('\n\t Time Period: %s\n', timePeriods{time_ind});
+%     for model_ind = 1:length(model)
+%         fprintf('\n Model: %s\n', model{model_ind});
+%         GAMcluster(model{model_ind}, timePeriods{time_ind}, main_dir, 'numFolds', numFolds, 'overwrite', isOverwrite, 'ridgeLambda', ridgeLambda, 'includeTimeBeforeZero', true);        
+%     end
+% end
