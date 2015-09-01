1025bf6d9d4eedf23496934aa332b8b71847b847
edeno <edeno@bu.edu>
2015-09-01 17:47:06 -0400

    Update GAMpred functions

--- Spike Prediction/collectGAMpred.m
+++ Spike Prediction/collectGAMpred.m
@@ -1,6 +1,6 @@
-function collectGAMpred(regressionModel_str, timePeriod, varargin)
+function collectGAMfit(modelFolderName, timePeriod, varargin)
 %%
-main_dir = '/data/home/edeno/Task Switching Analysis';
+main_dir = getenv('MAIN_DIR');
 load(sprintf('%s/paramSet.mat', main_dir), ...
     'data_info', 'validFolders', 'session_names');
 
@@ -9,7 +9,7 @@ inParser.addRequired('regressionModel_str', @ischar);
 inParser.addRequired('timePeriod',  @(x) any(ismember(x, validFolders)));
 inParser.addParamValue('overwrite', false, @islogical)
 
-inParser.parse(regressionModel_str, timePeriod, varargin{:});
+inParser.parse(modelFolderName, timePeriod, varargin{:});
 
 % Add parameters to input structure after validation
 params = inParser.Results;
@@ -19,19 +19,20 @@ timePeriod_dir = sprintf('%s/%s', data_info.processed_dir, timePeriod);
 data_dir = sprintf('%s/Models/%s', timePeriod_dir, params.regressionModel_str);
 
 cd(data_dir);
-GAMpred_names = strcat(session_names, '_GAMpred2.mat');
+GAMpred_names = strcat(session_names, '_GAMpred.mat');
 neurons_all = [];
-
-fprintf('\nModel: %s\n', regressionModel_str);
+numSim = 1000;
 
 for files_ind = 1:length(GAMpred_names),
-    fprintf('\t...Session: %s\n', GAMpred_names{files_ind});
-    file_name = sprintf('%s/%s', data_dir, GAMpred_names{files_ind});
+    
     try
-        load(file_name, 'neurons', 'gam', 'gamParams');
+        file_name = sprintf('%s/%s', data_dir, GAMpred_names{files_ind});
+        load(file_name, 'neurons', 'gam', 'gamParams', 'numNeurons', 'validPredType');
+        fprintf('\t...Session: %s\n', GAMpred_names{files_ind});
     catch
         continue;
     end
+    
     neurons = rmfield(neurons, 'stats');
     neurons_all = [neurons_all neurons];
 end
@@ -44,10 +45,20 @@ if ~exist(save_dir, 'dir'),
     mkdir(save_dir);
 end
 
-save_file_name = sprintf('%s/neurons2.mat', save_dir);
+save_file_name = sprintf('%s/neurons.mat', save_dir);
+
+[~, hostname] = system('hostname');
+hostname = strtrim(hostname);
 try
-    saveMillerlab('edeno', save_file_name, 'neurons', ...
-        'gam', 'gamParams', '-v7.3');
+    if strcmp(hostname, 'millerlab'),
+        saveMillerlab('edeno', save_file_name, 'neurons', ...
+            'gam', 'gamParams', 'validPredType', '-v7.3');
+    else
+        save(save_file_name, 'neurons', ...
+            'gam', 'gamParams', 'validPredType', '-v7.3');
+    end
 catch
+    fprintf('Model does not exist');
 end
+
 end
--- Spike Prediction/runCollectGAMpred.m
+++ Spike Prediction/runCollectGAMpred.m
@@ -1,16 +1,29 @@
+%% Collects GAMfit session files into one file
 clear all; close all; clc;
-main_dir = '/data/home/edeno/Task Switching Analysis';
-load(sprintf('%s/paramSet.mat', main_dir), 'data_info');
-timePeriod = 'Rule Response';
+setMainDir;
+main_dir = getenv('MAIN_DIR');
+load(sprintf('%s/paramSet.mat', main_dir), 'data_info', 'validFolders');
+jobMan = parcluster();
+isOverwrite = false;
 
-models_dir = sprintf('%s/%s/Models', data_info.processed_dir, timePeriod);
-
-models = dir(models_dir);
-models = {models.name};
-models = models(~ismember(models, {'.', '..'}));
-
-for models_ind = 1:length(models),
-   
-    collectGAMpred(models{models_ind}, timePeriod);
+for time_ind = 1:length(validFolders),
+    fprintf('\nTime Period: %s\n', validFolders{time_ind});
+    models_dir = sprintf('%s/%s/Models', data_info.processed_dir, validFolders{time_ind});
     
+    models = dir(models_dir);
+    models = {models([models.isdir]).name};
+    models = models(~ismember(models, {'.', '..'}));
+    
+    for models_ind = 1:length(models),
+        fprintf('\tModel: %s\n', models{models_ind});
+        if ~strcmp(strtrim(hostname), 'cns-ws18'),
+            gamJob{models_ind} = createCommunicatingJob(jobMan, 'AdditionalPaths', {data_info.script_dir}, 'AttachedFiles', ...
+                {which('saveMillerlab')}, 'NumWorkersRange', [12 12], 'Type', 'Pool');
+            createTask(gamJob{models_ind}, @collectGAMpred, 0, ...
+                {models{models_ind}, validFolders{time_ind}, 'overwrite', isOverwrite});
+            submit(gamJob{models_ind});
+        else
+            collectGAMpred(models{models_ind}, validFolders{time_ind}, 'overwrite', isOverwrite);
+        end
+    end
 end
