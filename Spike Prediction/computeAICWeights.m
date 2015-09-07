bcb6085d883470f50782271494973dda416ff887
edeno <edeno@bu.edu>
2015-09-07 15:26:51 -0400

    Move to analysis folder

--- Spike Prediction/computeAICWeights.m
+++
@@ -1,8 +0,0 @@
-function [weightsAIC, diffAIC, bestAIC_ind] = computeAICWeights(listAIC)
-
-[bestAIC, bestAIC_ind] = min(listAIC, [], 1);
-diffAIC = listAIC - bestAIC;
-relativeLikelihood = exp(-0.5 * diffAIC);
-weightsAIC = relativeLikelihood ./ sum(relativeLikelihood);
-
-end
