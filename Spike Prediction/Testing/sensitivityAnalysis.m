284970050faada715321e3e6667b8fd91609e71f
edeno <edeno@bu.edu>
2015-09-08 11:34:07 -0400

    Add heatmap plots of effect size vs number of trials

--- Spike Prediction/Testing/sensitivityAnalysis.m
+++ Spike Prediction/Testing/sensitivityAnalysis.m
@@ -59,3 +59,54 @@ end
 save('sensitivityAnalysis.mat', 'AUC', 'MI', 'ruleRatio', 'numSamples', ...
     'numTrials', 'orientRate', 'colorRate', 'numFolds', 'ridgeLambda')
 %% Plot
+colorMap_AUC = [254,240,217;
+    253,212,158;
+    253,187,132;
+    252,141,89;
+    239,101,72;
+    215,48,31;
+    153,0,0] ./ 255;
+ticks_AUC = 0:max(AUC(:)) / size(colorMap_AUC, 1):max(AUC(:));
+
+% AUC
+f1 = figure;
+imagesc(flipud(squeeze(mean(AUC, 1)))); 
+colormap(f1, colorMap_AUC);
+caxis([0, max(AUC(:))])
+cbr1 = colorbar;
+set(cbr1,'YTick', ticks_AUC);
+set(gca,'YDir','normal')
+box off;
+xlabel('Multiplicative Effect Size')
+set(gca, 'XTick', 1:length(orientRate))
+set(gca, 'XTickLabel', orientRate);
+ylabel('Number of Trials');
+set(gca, 'YTick', 1:length(numTrials))
+set(gca, 'YTickLabel', numTrials);
+title(sprintf('Difference of AUC\nbetween correct and misspecified model'));
+
+% Mutual Information
+colorMap_MI = [237,248,233;
+    199,233,192;
+    161,217,155;
+    116,196,118;
+    65,171,93;
+    35,139,69;
+    0,90,50] ./ 255;
+ticks_MI = 0:max(MI(:)) / size(colorMap_MI, 1):max(MI(:));
+
+f2 = figure;
+imagesc(flipud(squeeze(mean(MI, 1)))); 
+colormap(f2, colorMap_MI);
+caxis([0, max(MI(:))])
+cbr2 = colorbar;
+set(cbr2,'YTick', ticks_MI);
+set(gca,'YDir','normal')
+box off;
+xlabel('Multiplicative Effect Size')
+set(gca, 'XTick', 1:length(orientRate))
+set(gca, 'XTickLabel', orientRate);
+ylabel('Number of Trials');
+set(gca, 'YTick', 1:length(numTrials))
+set(gca, 'YTickLabel', numTrials);
+title(sprintf('Difference of MI\nbetween correct and misspecified model'));
