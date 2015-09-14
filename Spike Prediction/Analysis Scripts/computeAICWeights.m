be246d4968ac4e9698049fabfa502c4d54cb0504
edeno <edeno@bu.edu>
2015-09-14 17:58:49 -0400

    Allow computing weights on arbitrary dimension

--- Spike Prediction/Analysis Scripts/computeAICWeights.m
+++ Spike Prediction/Analysis Scripts/computeAICWeights.m
@@ -1,8 +1,7 @@
-function [weightsAIC, diffAIC, bestAIC_ind] = computeAICWeights(listAIC)
-
-[bestAIC, bestAIC_ind] = min(listAIC, [], 1);
-diffAIC = listAIC - bestAIC;
+function [weightsAIC, diffAIC, bestAIC_ind] = computeAICWeights(listAIC, dim)
+% dim corresponds to dimension containing models
+[bestAIC, bestAIC_ind] = min(listAIC, [], dim);
+diffAIC = bsxfun(@minus, listAIC, bestAIC);
 relativeLikelihood = exp(-0.5 * diffAIC);
-weightsAIC = relativeLikelihood ./ sum(relativeLikelihood);
-
+weightsAIC = bsxfun(@rdivide, relativeLikelihood,  nansum(relativeLikelihood, dim));
 end
