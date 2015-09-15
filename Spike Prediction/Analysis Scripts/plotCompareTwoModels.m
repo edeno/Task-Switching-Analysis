04020096a1b65ba5999a90410ba074ec934df8d3
edeno <edeno@bu.edu>
2015-09-15 18:28:33 -0400

    Hard encode plot limtis

--- Spike Prediction/Analysis Scripts/plotCompareTwoModels.m
+++ Spike Prediction/Analysis Scripts/plotCompareTwoModels.m
@@ -6,7 +6,6 @@ predType_ind = ismember(validPredType, predType);
 subplot = @(m,n,p) subtightplot (m, n, p, [0.09 0.09], [0.1 0.1], [0.1 0.1]);
 
 vsPredError = meanPredError(:, model_ind, isPFC == strcmp(brainArea, 'dlPFC'), predType_ind);
-limits = quantile(vsPredError(:), [0 1]);
 
 figure;
 for time_ind = 1:numTimePeriods,
@@ -17,16 +16,24 @@ for time_ind = 1:numTimePeriods,
         scatter(squeeze(vsPredError(time_ind, 3, :)), squeeze(vsPredError(time_ind, 4, :)));
     end
     axis square;
-    xlim(limits);
-    ylim(limits);
+    
     switch predType
         case 'MI'
+            limits = [-1 1];
+            xlim(limits);
+            ylim(limits);
             hline(0);
             vline(0);
         case 'AUC'
+            limits = [0.45 0.7];
+            xlim(limits);
+            ylim(limits);
             hline(0.5);
             vline(0.5);
         otherwise
+            limits = [0 1];
+            xlim(limits);
+            ylim(limits);
     end
     line(limits, limits);
     title(timePeriodNames(time_ind));
