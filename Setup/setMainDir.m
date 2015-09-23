5861a6c4d90c272093adf3cfb04fcd770a39ef0f
edeno <edeno@bu.edu>
2015-09-23 11:43:56 -0400

    Add host

--- Setup/setMainDir.m
+++ Setup/setMainDir.m
@@ -4,8 +4,9 @@ switch strtrim(hostname)
     case 'cns-ws18'
         setenv('MAIN_DIR', 'C:\Users\edeno\Task Switching Analysis\');
     case 'millerlab'
-        setenv('MAIN_DIR', '/data/home/edeno/Task Switching Analysis');
-    case 'mac'
+        setenv('MAIN_DIR', '/data/home/edeno/Task Switching Analysis/');
+    case 'Erics-MacBook-Air.local'
+        setenv('MAIN_DIR', '/Users/edeno/Documents/Task Switching Analysis/');
     otherwise
         setenv('MAIN_DIR', pwd);
 end
