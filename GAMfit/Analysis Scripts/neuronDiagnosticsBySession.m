67e7a69f6b1df901316542348fba0b3a4222fd1f
edeno <edeno@bu.edu>
2015-10-13 14:24:17 -0400

    Downsample if too many spikes/ISIs/model fits

--- GAMfit/Analysis Scripts/neuronDiagnosticsBySession.m
+++ GAMfit/Analysis Scripts/neuronDiagnosticsBySession.m
@@ -1,26 +1,38 @@
 fR = exp(designMatrix * gam.constraints' * [neurons.par_est]) * 1000;
 for neuronInd = 1:length(neurons),
     
-    uniformCDFvalues = stats(neuronInd).timeRescale.uniformCDFvalues;
-    uniformRescaledISIs = stats(neuronInd).timeRescale.uniformRescaledISIs;
-    normalRescaledISIs = stats(neuronInd).timeRescale.normalRescaledISIs;
-    rescaledISIs = sort(stats(neuronInd).timeRescale.rescaledISIs, 'ascend');
     numSpikes = stats(neuronInd).timeRescale.numSpikes;
+    if numSpikes > 2000,
+        % Downsample
+        sample_ind = sort(randperm(numSpikes, 2000));
+    else
+        sample_ind = 1:numSpikes;
+    end
+    uniformCDFvalues = stats(neuronInd).timeRescale.uniformCDFvalues(sample_ind);
+    uniformRescaledISIs = stats(neuronInd).timeRescale.uniformRescaledISIs(sample_ind);
+    normalRescaledISIs = stats(neuronInd).timeRescale.normalRescaledISIs(sample_ind);
+    sortedKS = stats(neuronInd).timeRescale.sortedKS(sample_ind);
+    rescaledISIs = sort(stats(neuronInd).timeRescale.rescaledISIs(sample_ind), 'ascend');
     ksCI = 1.36 / sqrt(numSpikes);
     corrCI = 1.96 / sqrt(numSpikes);
     [coef, lags] = xcorr(normalRescaledISIs(~isinf(normalRescaledISIs)), 'coeff');
-        
+    
     figure;
     subplot(2,3,1:2);
-    plot(gam.trial_time, fR(:, neuronInd), '.');
+    uniqueFR = unique([gam.trial_time fR(:, neuronInd)], 'rows');
+    p  = plot(uniqueFR(:, 1), uniqueFR(:, 2), '.');
     box off;
+    if max(uniqueFR(:, 2)) > 20,
+        maxY = max(uniqueFR(:, 2));
+    else
+        maxY = 20;
+    end
+    ylim([0 maxY]);
     xlabel('Time (ms)');
     ylabel('Firing Rate (sp / s)');
     
     subplot(2,3,3);
-    
-    
-    plot(uniformCDFvalues, stats(neuronInd).timeRescale.sortedKS - uniformCDFvalues); hold all;
+    plot(uniformCDFvalues, sortedKS - uniformCDFvalues); hold all;
     ylabel('Model CDF - Empirical CDF');
     xlabel('Quantiles');
     title('KS Test');
@@ -30,6 +42,7 @@ for neuronInd = 1:length(neurons),
     
     subplot(2,3,4);
     plot(uniformRescaledISIs(1:end-1), uniformRescaledISIs(2:end), '.');
+    alpha(0.5);
     xlabel('k - 1'); ylabel('k');
     box off;
     title('Consecutive Intervals of Uniform ISIs');
@@ -37,6 +50,8 @@ for neuronInd = 1:length(neurons),
     subplot(2,3,5);
     
     plot(lags, coef, '.');
+    alpha(0.5);
+    xlim(lags([1 end]));
     hline([-corrCI corrCI], 'k--');
     hline(0, 'k-');
     box off;
@@ -47,7 +62,7 @@ for neuronInd = 1:length(neurons),
     subplot(2,3,6);
     plot(rescaledISIs, expinv(uniformCDFvalues), '.')
     ylabel('Expected Theorectical ISI Quantiles');
-    xlabel('Observed ISIs');
+    xlabel('Observed ISI Quantiles');
     lineHandle = line([0 max(rescaledISIs)], [0 max(rescaledISIs)]);
     lineHandle.Color = 'black';
     axis([0 max(rescaledISIs) 0 max(rescaledISIs)]);
@@ -58,5 +73,7 @@ for neuronInd = 1:length(neurons),
         neurons(neuronInd).brainArea, ...
         neurons(neuronInd).session_name, ...
         neurons(neuronInd).wire_number, ...
-        neurons(neuronInd).unit_number), gam.model_str, gamParams.timePeriod})
+        neurons(neuronInd).unit_number), ...
+        sprintf('Model: %s', gam.model_str), ...
+        sprintf('Time Period: %s', gamParams.timePeriod)});
 end
