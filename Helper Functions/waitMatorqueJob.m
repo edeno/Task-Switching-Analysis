cf9278004002f77156b632858aa95642d71057ae
edeno <edeno@bu.edu>
2015-10-15 15:25:18 -0400

    Add in informative statements and option for timeout

--- Helper Functions/waitMatorqueJob.m
+++ Helper Functions/waitMatorqueJob.m
@@ -1,13 +1,24 @@
-function waitMatorqueJob(job)
-timeout = inf;
+function waitMatorqueJob(job, varargin)
+
+inParser = inputParser;
+inParser.addRequired('job', @isobject);
+inParser.addParameter('timeout', inf, @(x) x > 0 && isnumeric(x)); % in seconds
+
+inParser.parse(job, varargin{:});
+
+timeout = inParser.Results.timeout;
 try
     startTime = tic;
-    fprintf('Waiting for %s to finish', job.dir);
+    fprintf('Waiting for %s to finish...\n', job.dir);
     while true
         % Lets check if we have already reached or gone beyond the requested state
         isDone = strcmp(job.status, 'done');
-        if isDone || toc(startTime) > timeout
-            break
+        if isDone
+             fprintf('\t...Job finished!\n');
+            break;
+        elseif toc(startTime) > timeout
+             break;
+             fprintf('\tJob exceeded timeout, %d!\n', timeout);
         end
         pause(1);
     end
