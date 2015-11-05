d078da8a7ae802d8a79ebe51b100789733f42d22
edeno <edeno@bu.edu>
2015-11-05 16:53:54 -0500

    Some code to run standalone executable on cluster

---
+++ GAMfit/GAMCluster_standalone.m
@@ -0,0 +1,49 @@
+function GAMCluster_standalone(session_ind, regressionModel_str, timePeriod, varargin)
+% Standalone Main Program Rules
+% a) It must be a function, and it must not have output
+%    >> myStandalone 2000 4       % command  syntax
+%    >> myStandalone(2000, 4)     % function syntax
+% b) The standalone runs in command syntax only
+%    scc1$ ./myExecR2013a 2000 4
+% c) Commandline input (if any) are passed as strings
+
+% Specify number of processors
+NPROCS = 12;
+% Must be converted to double for used as double
+session_ind = str2double(session_ind);
+if ~isempty(varargin),
+    varargin{2:2:end} = str2double(varargin{2:2:end});
+end
+%% Validate Parameters
+main_dir = getWorkingDir();
+
+% Load Common Parameters
+load(sprintf('%s/paramSet.mat', main_dir), ...
+    'covInfo', 'timePeriodNames', 'sessionNames', ...
+    'monkeyNames', 'validPredType');
+
+inParser = inputParser;
+inParser.addRequired('regressionModel_str', @ischar);
+inParser.addRequired('timePeriod',  @(x) any(ismember(x, timePeriodNames)));
+inParser.addParameter('numFolds', 5, @(x) isnumeric(x) && x > 0)
+inParser.addParameter('predType', 'Dev', @(x) any(ismember(x, validPredType)))
+inParser.addParameter('smoothLambda', 10.^(-3:4), @isvector)
+inParser.addParameter('ridgeLambda', 1, @isvector)
+inParser.addParameter('overwrite', false, @isnumeric)
+inParser.addParameter('includeIncorrect', false, @isnumeric);
+inParser.addParameter('includeFixationBreaks', false, @isnumeric);
+inParser.addParameter('includeTimeBeforeZero', true, @isnumeric);
+inParser.addParameter('isPrediction', false, @isnumeric);
+
+inParser.parse(regressionModel_str, timePeriod, varargin{:});
+
+% Add parameters to input structure after validation
+gamParams = inParser.Results;
+
+% Compute sum with Parallel Computing Toolbox's parfor
+matlabpool('local', NPROCS);  % R2013a or older
+ComputeGAMfit(sessionNames{session_ind}, gamParams, covInfo);
+matlabpool close;
+exit;
+
+end
