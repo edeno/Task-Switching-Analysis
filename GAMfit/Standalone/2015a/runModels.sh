0a6db546d6fc0cbe608760ba5410a36dbb880955
edeno <edeno@bu.edu>
2015-11-08 23:37:50 -0500

    Try to fix parameter passing

--- GAMfit/Standalone/2015a/runModels.sh
+++ GAMfit/Standalone/2015a/runModels.sh
@@ -8,17 +8,18 @@ printf "\n\nProcessing Time Period: %s \n" "$timeperiod"
 
 for (( i = 0; i < ${#modelList[@]}; i++ ))
 do
-  printf "\tProcessing Model: %s \n" "${modelList[$i]}"
+  curModel="${modelList[$i]}"
+  printf "\tProcessing Model: %s \n" $curModel
   # Update model list
   matlab -nodisplay -r "gamParams.timePeriod = '$timeperiod'; \
-  gamParams.regressionModel_str = '${modelList[$i]}'; \
+  gamParams.regressionModel_str = '$curModel'; \
   addpath('/projectnb/pfc-rule/Task-Switching-Analysis/Helper Functions'); \
   updateModelList(gamParams); exit;"
   # Submit Cluster Jobs
   qsub -t 1-2 \
        -N GAMfit \
        -l h_rt=24:00:00 \
-       -v MODEL="${modelList[$i]}" \
+       -v MODEL="$curModel" \
        -v TIMEPERIOD="$timeperiod" \
        -v INCLUDETIMEBEFOREZERO="1" \
        -v SMOOTHLAMBDA="10.^(-3:4)" \
