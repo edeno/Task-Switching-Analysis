47d65ab8a9513dcc9be2e8429d8acac5dd9f1b74
edeno <edeno@bu.edu>
2015-11-08 19:25:32 -0500

    Remove memory total
    Cluster not accepting value. Need to figure out why.

--- GAMfit/Standalone/2015a/runModels.sh
+++ GAMfit/Standalone/2015a/runModels.sh
@@ -2,8 +2,6 @@
 modelList=("s(Previous Error, Trial Time, knotDiff=50) + s(Response Direction, Trial Time, knotDiff=50)"
 "s(Previous Error, Trial Time, knotDiff=50) + s(Response Direction, Trial Time, knotDiff=50) + s(Rule Repetition, Trial Time, knotDiff=50)")
 
-runScript="./runGAMCluster2015a.sh"
-
 # Time Period: Rule Response
 timeperiod="Rule Response"
 printf "\n\nProcessing Time Period: %s \n" "$timeperiod"
@@ -20,12 +18,10 @@ do
   qsub -t 1-2 \
        -N GAMfit \
        -l h_rt=24:00:00 \
-       -l mem_total=94G \
        -v MODEL="${modelList[$i]}" \
        -v TIMEPERIOD="$timeperiod" \
        -v INCLUDETIMEBEFOREZERO="1" \
        -v SMOOTHLAMBDA="10.^(-3:4)" \
        -j y \
-       -w v \
-       "$runScript" \;
+       ./runGAMCluster2015a.sh;
 done
