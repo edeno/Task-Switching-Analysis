dc5aa33cc857af99abadc45eedd811f45130dc87
edeno <edeno@bu.edu>
2016-02-18 18:23:38 -0500

    Add stimulus reward time period

--- Spike Prediction/Analysis Scripts/runCompareModelPredictions.m
+++ Spike Prediction/Analysis Scripts/runCompareModelPredictions.m
@@ -1,3 +1,5 @@
 %% runCompareModelPredictions
 CompareModelPredictions('pred_ACC_Rule Response_constant.mat')
 CompareModelPredictions('pred_dlPFC_Rule Response_constant.mat')
+CompareModelPredictions('pred_ACC_Stimulus Reward_constant.mat')
+CompareModelPredictions('pred_dlPFC_Stimulus Reward_constant.mat')
