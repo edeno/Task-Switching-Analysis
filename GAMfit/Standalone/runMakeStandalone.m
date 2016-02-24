7b31eb273def2e1097fb7d2deded62086e89ec2d
edeno <edeno@bu.edu>
2016-02-24 08:16:19 -0500

    Make sure standalone saves in the standalone directory

--- GAMfit/Standalone/runMakeStandalone.m
+++ GAMfit/Standalone/runMakeStandalone.m
@@ -1,5 +1,6 @@
 %% Compiles the standalone
 workingDir = '/projectnb/pfc-rule/Task-Switching-Analysis/';
 addpath(genpath(workingDir));
+cd(sprintf('%s/GAMfit/Standalone', workingDir));
 make GAMCluster_makefile
 delete('run_GAMClusterExec*.sh')
