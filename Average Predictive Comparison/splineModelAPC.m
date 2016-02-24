55f6ccc37685b794f5eee7da06b07ad5ebbfdb04
edeno <edeno@bu.edu>
2016-02-24 08:18:01 -0500

    Update model

--- Average Predictive Comparison/splineModelAPC.m
+++ Average Predictive Comparison/splineModelAPC.m
@@ -2,12 +2,12 @@ clear variables;
 numSim = 1000;
 numSamples = [];
 overwrite = false;
-timePeriods = {'Rule Response', 'Stimulus Reward'};
+timePeriods = {'Rule Response'};
 walltime = '24:00:00';
 mem = '90GB';
 numCores = 12;
 model = {...
-    's(Previous Error, Trial Time, knotDiff=50) + s(Response Direction, Trial Time, knotDiff=50)', ...
+    's(Previous Error, Trial Time, knotDiff=50) + s(Response Direction, Trial Time, knotDiff=50) + s(Rule, Trial Time, knotDiff=50)', ...
     };
 
 runAPC;
