5ece2f012c91306667ec0ca580ebe886773b23bc
edeno <edeno@bu.edu>
2016-02-25 11:01:27 -0500

    Reduce number of cores

--- GAMfit/Standalone/runModelsPred.sh
+++ GAMfit/Standalone/runModelsPred.sh
@@ -33,6 +33,6 @@ do
        -v OVERWRITE="0" \
        -v ISPREDICTION="1" \
        -v SMOOTHLAMBDA="10.^(-3:4)" \
-       -v NUMCORES="8" \
+       -v NUMCORES="5" \
        ./runGAMCluster2015a.sh;
 done
