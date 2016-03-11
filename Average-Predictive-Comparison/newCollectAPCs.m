371525b8a7f6d8aef82db46e65dfc6e7e260621b
Eric Denovellis <edeno@bu.edu>
2016-03-10 20:11:32 -0500

    Collect APC on scc cluster

---
+++ Average-Predictive-Comparison/newCollectAPCs.m
@@ -0,0 +1,30 @@
+workingDir = getWorkingDir();
+timePeriod = 'Rule Response';
+modelDir = sprintf('%s/Processed Data/%s/Models/', workingDir, timePeriod);
+load(sprintf('%s/modelList.mat', modelDir));
+model = 'Rule * Previous Error + Response Direction + Rule * Rule Repetition + Congruency';
+apcDir = sprintf('%s/%s/APC/', modelDir, modelList(model));
+
+folderNames = dir(apcDir);
+folderNames = {folderNames.name};
+folderNames = folderNames(~ismember(folderNames, {'.', '..'}));
+
+for folder_ind = 1:length(folderNames),
+    fprintf('\nFolder: %s\n', folderNames{folder_ind});
+    APC_files = dir(sprintf('%s/%s/*_APC.mat', apcDir, folderNames{folder_ind}));
+    APC_files = {APC_files.name};
+    avpred = [];
+
+    for file_ind = 1:length(APC_files),
+        fprintf('\t...%s\n', APC_files{file_ind})
+        curAPC = load(APC_files{file_ind});
+        avpred = [avpred, curAPC.avpred];
+    end
+
+    saveDir = sprintf('%s/%s/apcCollected/', apcDir, folderNames{folder_ind});
+    if ~exist(saveDir, 'dir'),
+        mkdir(saveDir);
+    end
+    fprintf('/nSaving.../n');
+    save(sprintf('%s/apcCollected.mat', saveDir), 'avpred', '-v7.3');
+end
