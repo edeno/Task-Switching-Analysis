bd33740891ebf515cb4346f4904b52b82b87005d
Eric Denovellis <edeno@bu.edu>
2016-03-12 13:56:53 -0500

    try moving the curModel

--- Average-Predictive-Comparison/Standalone/runAPCRuleBy.sh
+++ Average-Predictive-Comparison/Standalone/runAPCRuleBy.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 model="s(Rule * Previous Error, Trial Time, knotDiff=50) + s(Response Direction, Trial Time, knotDiff=50) + s(Rule * Rule Repetition, Trial Time, knotDiff=50) + s(Congruency, Trial Time, knotDiff=50)"
+# Escape commas in model string with single quotes
+# because qsub splits passed variables with commas
+curModel=$(echo "$model" | sed -e "s/,/','/g")
+printf "\tProcessing Model: %s \n" "$curModel"
 factorList[0]="Previous Error"
 factorList[1]="Rule Repetition"
 numFiles=34;
@@ -10,11 +14,7 @@ printf "\n\nProcessing Time Period: %s \n" "$timeperiod"
 for (( i = 0; i < ${#factorList[@]}; i++ ))
 do
   curFactor="${factorList[$i]}"
-  printf "\tProcessing Model: %s \n" "$curModel"
   printf "\t\tProcessing Factor: %s \n" "$curFactor"
-  # Escape commas in model string with single quotes
-  # because qsub splits passed variables with commas
-  curModel=$(echo "$model" | sed -e "s/,/','/g")
   # Submit Cluster Jobs
   qsub -t "1-$numFiles" \
        -N "APC_RuleBy$curFactor" \
