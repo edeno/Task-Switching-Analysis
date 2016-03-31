d2263835c4c39edf6a7afaf01e54bdff7e073259
Eric Denovellis <edeno@bu.edu>
2016-03-30 20:11:53 -0400

    Add code for saving figures

--- Permutation-Analysis/Analysis/plotPermutationAnalysis.m
+++ Permutation-Analysis/Analysis/plotPermutationAnalysis.m
@@ -13,9 +13,13 @@ filterComparison = @(x) ismember(values(1).comparisonNames, x);
 obsDiff = [values.obsDiff];
 normObsDiff = [values.normObsDiff];
 comparison_ind = filterComparison(comparisonName) & filterTimePeriod(timePeriod);
+saveDir = sprintf('%s/Figures/%s/', workingDir, timePeriod);
+if ~exist(saveDir, 'dir'),
+    mkdir(saveDir);
+end
 
 % Both monkeys
-figure;
+f = figure;
 for area_ind = 1:length(brainAreas),
     neuron_ind = filterArea(brainAreas{area_ind});
     sig_ind = h(comparison_ind, :);
@@ -61,11 +65,13 @@ for area_ind = 1:length(brainAreas),
     vline(meanAbsNormObsDiff, 'Label', sprintf('%.2f', meanAbsNormObsDiff));
     xlabel({'Sig. Only, Abs. Norm. Difference','(by Average Firing Rate)'});
 end
-suptitle(sprintf('Both Monkeys: %s', comparisonName))
+f.Name = sprintf('%s - Both Monkeys: %s', timePeriod, comparisonName);
+saveName = sprintf('%s/%s_Both Monkeys_%s', saveDir, timePeriod, comparisonName);
+saveas(f, saveName);
 
 % Individual monkeys
 for monkey_ind = 1:length(monkeyNames),
-    figure;
+    f = figure;
     for area_ind = 1:length(brainAreas),
         neuron_ind = filterArea(brainAreas{area_ind}) & filterMonkey(monkeyNames{monkey_ind});
         sig_ind = h(comparison_ind, :);
@@ -103,7 +109,7 @@ for monkey_ind = 1:length(monkeyNames),
         hist(normObsDiff(comparison_ind, neuron_ind & sig_ind), 50);
         meanNormObsDiff = mean(normObsDiff(comparison_ind, neuron_ind & sig_ind), 2);
         vline(meanNormObsDiff, 'Label', sprintf('%.2f', meanNormObsDiff))
-        xlabel({'Sig. Only, Norm. Difference (by Average Firing Rate)'});
+        xlabel({'Sig. Only, Norm. Difference','(by Average Firing Rate)'});
         
         subplot(6,2, 11 + (area_ind - 1));
         hist(abs(normObsDiff(comparison_ind, neuron_ind & sig_ind)), 50);
@@ -111,36 +117,38 @@ for monkey_ind = 1:length(monkeyNames),
         vline(meanAbsNormObsDiff, 'Label', sprintf('%.2f', meanAbsNormObsDiff));
         xlabel({'Sig. Only, Abs. Norm. Difference','(by Average Firing Rate)'});
     end
-    suptitle(sprintf('Monkey %s: %s', monkeyNames{monkey_ind}, comparisonName))
+    f.Name = sprintf('%s - Monkey %s: %s', timePeriod, monkeyNames{monkey_ind}, comparisonName);
+    saveName = sprintf('%s/%s_Monkey %s_%s', saveDir, timePeriod, monkeyNames{monkey_ind}, comparisonName);
+    saveas(f, saveName);
 end
 
 %%
-avgFiringRate = [values.avgFiringRate];
-figure;
-for area_ind = 1:length(brainAreas),
-    
-    subplot(3, 2, 1 + (area_ind - 1));
-    neuron_ind = filterArea(brainAreas{area_ind});
-    hist(avgFiringRate(neuron_ind), 50);
-    vline(quantile(avgFiringRate(neuron_ind), [.25 .5 .75]))
-    xlim([0 max(avgFiringRate)])
-    title(brainAreas{area_ind});
-    
-    subplot(3, 2, 3 + (area_ind - 1));
-    neuron_ind = filterArea(brainAreas{area_ind}) & filterMonkey(monkeyNames{1});
-    hist(avgFiringRate(neuron_ind), 50);
-    vline(quantile(avgFiringRate(neuron_ind), [.25 .5 .75]))
-    xlim([0 max(avgFiringRate)])
-    title(monkeyNames{1});
-    
-    subplot(3, 2, 5 + (area_ind - 1));
-    neuron_ind = filterArea(brainAreas{area_ind}) & filterMonkey(monkeyNames{2});
-    hist(avgFiringRate(neuron_ind), 50);
-    vline(quantile(avgFiringRate(neuron_ind), [.25 .5 .75]))
-    xlim([0 max(avgFiringRate)])
-    title(monkeyNames{2});
-end
-suptitle(sprintf('%s: Average Firing Rate', timePeriod));
+% avgFiringRate = [values.avgFiringRate];
+% f = figure;
+% for area_ind = 1:length(brainAreas),
+%
+%     subplot(3, 2, 1 + (area_ind - 1));
+%     neuron_ind = filterArea(brainAreas{area_ind});
+%     hist(avgFiringRate(neuron_ind), 50);
+%     vline(quantile(avgFiringRate(neuron_ind), [.25 .5 .75]))
+%     xlim([0 max(avgFiringRate)])
+%     title(brainAreas{area_ind});
+%
+%     subplot(3, 2, 3 + (area_ind - 1));
+%     neuron_ind = filterArea(brainAreas{area_ind}) & filterMonkey(monkeyNames{1});
+%     hist(avgFiringRate(neuron_ind), 50);
+%     vline(quantile(avgFiringRate(neuron_ind), [.25 .5 .75]))
+%     xlim([0 max(avgFiringRate)])
+%     title(monkeyNames{1});
+%
+%     subplot(3, 2, 5 + (area_ind - 1));
+%     neuron_ind = filterArea(brainAreas{area_ind}) & filterMonkey(monkeyNames{2});
+%     hist(avgFiringRate(neuron_ind), 50);
+%     vline(quantile(avgFiringRate(neuron_ind), [.25 .5 .75]))
+%     xlim([0 max(avgFiringRate)])
+%     title(monkeyNames{2});
+% end
+% f.Name = sprintf('%s: Average Firing Rate', timePeriod);
 
 
 end
