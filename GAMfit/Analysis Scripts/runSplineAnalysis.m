8a3144951f59b8d3f3249917d8a32a9fd2b67181
Eric Denovellis <edeno@bu.edu>
2016-04-23 19:27:14 -0400

    Some starter code for analyzing the splines

---
+++ GAMfit/Analysis Scripts/runSplineAnalysis.m
@@ -0,0 +1,52 @@
+timePeriod = 'Rule Stimulus';
+model = 's(Rule, Trial Time, knotDiff=50) + s(Previous Error, Trial Time, knotDiff=50) + s(Rule Repetition, Trial Time, knotDiff=50)';
+[timeEst, time] = getSplineCoef(model, timePeriod, 'brainArea', 'dlPFC', 'isSim', true, 'subject', 'isa');
+
+prevError = cat(4, timeEst.Previous_Error);
+ruleRep = cat(4, timeEst.Rule_Repetition);
+rule = cat(4, timeEst.Rule);
+ruleRep(abs(ruleRep) > 10) = NaN;
+
+stat = @(s) nanmean(s, 4);
+bootEst = @(s) quantile(s, [0.025, 0.5, 0.975], 3);
+
+est = squeeze(exp(bootEst(stat(ruleRep))));
+
+if strcmp(unique({timeEst.subject}), 'isa'),
+    bad_ind = time > 346;
+    prevError = prevError(:, ~bad_ind, :, :);
+    ruleRep = ruleRep(:, ~bad_ind, :, :);
+    rule = rule(:, ~bad_ind, :, :);
+    time = time(~bad_ind);
+    est = est(:, ~bad_ind, :);
+end
+
+%%
+figure;
+subplot(1,3,1);
+for k = 1:4,
+    plot(time, squeeze(est(k, :, :))); hold all;
+end
+title('Rule Repetition');
+xlim(quantile(time, [0 1]));
+ylim([0.6 1.4]);
+hline(1, 'Color', 'black', 'LineType', '-');
+vline(0, 'Color', 'black', 'LineType', '-');
+
+
+subplot(1,3,2);
+plot(time, exp(squeeze(bootEst(stat(abs(rule))))));
+title('Rule');
+ylim([0.6 1.4]);
+xlim(quantile(time, [0 1]));
+vline(0, 'Color', 'black', 'LineType', '-');
+hline(1, 'Color', 'black', 'LineType', '-');
+
+
+subplot(1,3,3);
+plot(time, exp(squeeze(bootEst(stat(prevError)))));
+title('Previous Error');
+ylim([0.6 1.4]);
+xlim(quantile(time, [0 1]));
+vline(0, 'Color', 'black', 'LineType', '-');
+hline(1, 'Color', 'black', 'LineType', '-');
