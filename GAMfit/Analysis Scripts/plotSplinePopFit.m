08ca003c35b1f900a1c43dc3f74a79d6f75d43ae
Eric Denovellis <edeno@bu.edu>
2016-04-24 14:20:04 -0400

    Separate plotting into a different function

---
+++ GAMfit/Analysis Scripts/plotSplinePopFit.m
@@ -0,0 +1,142 @@
+function plotSplinePopFit(model, timePeriod, varargin)
+inParser = inputParser;
+inParser.addRequired('model', @ischar);
+inParser.addRequired('timePeriod', @ischar);
+inParser.addParameter('subject', '*', @ischar);
+inParser.addParameter('brainArea', '*', @ischar);
+
+inParser.parse(model, timePeriod, varargin{:});
+params = inParser.Results;
+
+stat = @(s) nanmean(s, 4);
+bootEst = @(s) quantile(s, [0.025, 0.5, 0.975], 3);
+
+[timeEst, time] = getSplineCoef(model, timePeriod, 'brainArea', params.brainArea, 'isSim', true, 'subject', params.subject);
+
+prevError = cat(4, timeEst.Previous_Error);
+ruleRep = cat(4, timeEst.Rule_Repetition);
+rule = cat(4, timeEst.Rule);
+
+ruleRep(abs(ruleRep) > 10) = NaN;
+rule(abs(rule) > 10) = NaN;
+prevError(abs(prevError) > 10) = NaN;
+
+if strcmp(unique({timeEst.subject}), 'isa'),
+    bad_ind = time > 346;
+    prevError = prevError(:, ~bad_ind, :, :);
+    ruleRep = ruleRep(:, ~bad_ind, :, :);
+    rule = rule(:, ~bad_ind, :, :);
+    time = time(~bad_ind);
+    ruleRepEst = ruleRepEst(:, ~bad_ind, :);
+end
+
+colorOrder = [ ...
+    37,52,148; ...
+    44,127,184; ...
+    65,182,196; ...
+    127,205,187; ...
+    199,233,180; ...
+    ] ./ 255;
+
+%% Average Population Change
+f = figure;
+f.Name = sprintf('%s - %s - %s', params.brainArea, params.timePeriod, params.subject);
+subplot(3,3,1);
+plotPopMean(abs(rule))
+set(gca, 'YLim', log([0.9 1.2]));
+set(gca, 'YTick', log(0.9:0.05:1.2));
+set(gca, 'YTickLabel', ((0.9:0.05:1.2) - 1) * 100);
+title('Rule');
+
+subplot(3,3,2);
+plotPopMean(ruleRep)
+title('Rule Repetition');
+
+subplot(3,3,3);
+plotPopMean(prevError)
+title('Previous Error');
+
+%% Number of Significant by Time
+subplot(3,3,4);
+plotPopSig(rule)
+
+subplot(3,3,5);
+plotPopSig(ruleRep)
+
+subplot(3,3,6);
+plotPopSig(prevError)
+
+%% Time to first significance
+subplot(3,3,7);
+plotTimeToSig(rule)
+
+subplot(3,3,8);
+plotTimeToSig(ruleRep)
+
+subplot(3,3,9);
+plotTimeToSig(prevError)
+
+%%
+    function plotPopMean(est)
+        data = bootEst(stat(est));
+        numLevels = size(data, 1);
+        for level_ind = 1:numLevels,
+            plot(time, squeeze((data(level_ind, :, 2, :))), 'LineWidth', 2, 'Color', colorOrder(level_ind, :));
+            hold all;
+            plot(time, squeeze((data(level_ind, :, [1 3], :))), 'LineWidth', 0.5, 'Color', colorOrder(level_ind, :));
+        end
+        
+        set(gca, 'YLim', log([1 1.4]));
+        set(gca, 'YTick', log(1:0.05:1.4));
+        set(gca, 'YTickLabel', ((1:0.05:1.4) - 1) * 100);
+        ylim(log([1 1.4]));
+        xlim([-50, max(time)]);
+        vline(0, 'Color', 'black', 'LineType', '-');
+        hline(1, 'Color', 'black', 'LineType', '-');
+    end
+%%
+    function plotPopSig(est)
+        data = bootEst(est);
+        numLevels = size(data, 1);
+        h = data(:, :, 1, :) > 0 | data(:, :, 3, :) < 0;
+        plotHandle = plot(time, squeeze(mean(h, 4)) * 100);
+        colors = colorOrder(1:numLevels, :);
+        set(plotHandle, {'Color'}, num2cell(colors, 2));
+        xlim([-50, max(time)]);
+        ylim([0 60])
+        vline(0, 'Color', 'black', 'LineType', '-');
+    end
+%%
+    function plotTimeToSig(est)
+        data = bootEst(est);
+        numLevels = size(data, 1);
+        h = data(:, :, 1, :) > 0 | data(:, :, 3, :) < 0;
+        timeSig = nan(size(h, 4), numLevels);
+        NUM_CONSEC = 5;
+        
+        for level_ind = 1:numLevels,
+            for neuron_ind = 1:size(h, 4),
+                timeSig(neuron_ind, level_ind) = firstTimeSig(squeeze(h(level_ind, :, :, neuron_ind)), NUM_CONSEC);
+            end
+            plot(mean(timeSig(:, level_ind)), level_ind, '.', 'MarkerSize', 20, 'Color', colorOrder(level_ind, :))
+            hold all;
+            ci = ([-1 1] * 1.96 * std(timeSig(:, level_ind)) / sqrt(length(timeSig(:, level_ind)))) + mean(timeSig(:, level_ind)) * [1 1];
+            l = line(ci, [level_ind level_ind]);
+            l.Color = colorOrder(level_ind, :);
+            xlim([-50, max(time)]);
+        end
+        vline(0, 'Color', 'black', 'LineType', '-');
+        %%
+        function [sigTime] = firstTimeSig(x, numConsec)
+            time_ind = find(conv(double(x), ones(numConsec, 1)) == numConsec, 1) - (numConsec - 1);
+            if isempty(time_ind)
+                sigTime = time(end);
+            else
+                sigTime = time(time_ind);
+            end
+        end
+        
+    end
+
+end
+
--- GAMfit/Analysis Scripts/runSplineAnalysis.m
+++ GAMfit/Analysis Scripts/runSplineAnalysis.m
@@ -1,63 +1,15 @@
-timePeriod = 'Rule Stimulus';
-model = 's(Rule, Trial Time, knotDiff=50) + s(Previous Error, Trial Time, knotDiff=50) + s(Rule Repetition, Trial Time, knotDiff=50)';
-[timeEst, time] = getSplineCoef(model, timePeriod, 'brainArea', 'ACC', 'isSim', true, 'subject', 'isa');
+clear variables;
+timePeriods = {'Rule Stimulus', ...
+    'Stimulus Response'};
+models = {'s(Rule, Trial Time, knotDiff=50) + s(Previous Error, Trial Time, knotDiff=50) + s(Rule Repetition, Trial Time, knotDiff=50)', ...
+    's(Rule, Trial Time, knotDiff=50) + s(Previous Error, Trial Time, knotDiff=50) + s(Rule Repetition, Trial Time, knotDiff=50)'};
+subjects = {'cc', 'isa'};
+brainAreas = {'ACC', 'dlPFC'};
 
-prevError = cat(4, timeEst.Previous_Error);
-ruleRep = cat(4, timeEst.Rule_Repetition);
-rule = cat(4, timeEst.Rule);
-
-ruleRep(abs(ruleRep) > 10) = NaN;
-rule(abs(rule) > 10) = NaN;
-prevError(abs(prevError) > 10) = NaN;
-
-stat = @(s) nanmean(s, 4);
-bootEst = @(s) quantile(s, [0.025, 0.5, 0.975], 3);
-
-est = squeeze(exp(bootEst(stat(ruleRep))));
-
-if strcmp(unique({timeEst.subject}), 'isa'),
-    bad_ind = time > 346;
-    prevError = prevError(:, ~bad_ind, :, :);
-    ruleRep = ruleRep(:, ~bad_ind, :, :);
-    rule = rule(:, ~bad_ind, :, :);
-    time = time(~bad_ind);
-    est = est(:, ~bad_ind, :);
+for model_ind = 1:length(models),
+    for subject_ind = 1:length(subjects),
+        for area_ind = 1:length(brainAreas),
+            plotSplinePopFit(models{model_ind}, timePeriods{model_ind}, 'subject', subjects{subject_ind}, 'brainArea', brainAreas{area_ind})
+        end
+    end
 end
-
-colorOrder =  [ ...
-    228,26,28; ...
-    199,233,180; ...
-    127,205,187; ...
-    65,182,196; ...
-    199,233,180; ...
-    ] ./ 255;
-
-%%
-figure;
-subplot(1,3,1);
-for k = 1:4,
-    plot(time, squeeze(est(k, :, :)), 'Color', colorOrder(k, :), 'LineWidth', 2); hold all;
-end
-title('Rule Repetition');
-xlim(quantile(time, [0 1]));
-ylim([0.6 1.4]);
-hline(1, 'Color', 'black', 'LineType', '-');
-vline(0, 'Color', 'black', 'LineType', '-');
-
-
-subplot(1,3,2);
-plot(time, exp(squeeze(bootEst(stat(abs(rule))))));
-title('Rule');
-ylim([0.6 1.4]);
-xlim(quantile(time, [0 1]));
-vline(0, 'Color', 'black', 'LineType', '-');
-hline(1, 'Color', 'black', 'LineType', '-');
-
-
-subplot(1,3,3);
-plot(time, exp(squeeze(bootEst(stat(prevError)))));
-title('Previous Error');
-ylim([0.6 1.4]);
-xlim(quantile(time, [0 1]));
-vline(0, 'Color', 'black', 'LineType', '-');
-hline(1, 'Color', 'black', 'LineType', '-');
