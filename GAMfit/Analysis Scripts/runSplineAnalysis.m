c38d4fc4e2ef5822737b1bf08eed8eaac3d90cba
Eric Denovellis <edeno@bu.edu>
2016-04-24 10:12:55 -0400

    add colors

--- GAMfit/Analysis Scripts/runSplineAnalysis.m
+++ GAMfit/Analysis Scripts/runSplineAnalysis.m
@@ -1,11 +1,14 @@
 timePeriod = 'Rule Stimulus';
 model = 's(Rule, Trial Time, knotDiff=50) + s(Previous Error, Trial Time, knotDiff=50) + s(Rule Repetition, Trial Time, knotDiff=50)';
-[timeEst, time] = getSplineCoef(model, timePeriod, 'brainArea', 'dlPFC', 'isSim', true, 'subject', 'isa');
+[timeEst, time] = getSplineCoef(model, timePeriod, 'brainArea', 'ACC', 'isSim', true, 'subject', 'isa');
 
 prevError = cat(4, timeEst.Previous_Error);
 ruleRep = cat(4, timeEst.Rule_Repetition);
 rule = cat(4, timeEst.Rule);
+
 ruleRep(abs(ruleRep) > 10) = NaN;
+rule(abs(rule) > 10) = NaN;
+prevError(abs(prevError) > 10) = NaN;
 
 stat = @(s) nanmean(s, 4);
 bootEst = @(s) quantile(s, [0.025, 0.5, 0.975], 3);
@@ -21,11 +24,19 @@ if strcmp(unique({timeEst.subject}), 'isa'),
     est = est(:, ~bad_ind, :);
 end
 
+colorOrder =  [ ...
+    228,26,28; ...
+    199,233,180; ...
+    127,205,187; ...
+    65,182,196; ...
+    199,233,180; ...
+    ] ./ 255;
+
 %%
 figure;
 subplot(1,3,1);
 for k = 1:4,
-    plot(time, squeeze(est(k, :, :))); hold all;
+    plot(time, squeeze(est(k, :, :)), 'Color', colorOrder(k, :), 'LineWidth', 2); hold all;
 end
 title('Rule Repetition');
 xlim(quantile(time, [0 1]));
