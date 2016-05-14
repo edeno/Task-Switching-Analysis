f259718c8d2af04c004955f573fb870b42e04303
Eric Denovellis <edeno@bu.edu>
2016-05-14 12:14:45 -0400

    Add in mean change

--- GAMfit/Analysis Scripts/getSigTime.m
+++ GAMfit/Analysis Scripts/getSigTime.m
@@ -19,19 +19,21 @@ levelsOfInterest = levelsOfInterest(~ismember(levelsOfInterest, covInfo(covOfInt
 numLevels = length(levelsOfInterest);
 
 getTimeLevels = @(level) cellfun(@(x) ~isempty(x), regexp(levelNames, sprintf('%s.Trial Time.*', level)));
-% getMeanLevel = @(level) cellfun(@(x) ~isempty(x), regexp(levelNames, sprintf('%s$', level)));
+getMeanLevel = @(level) cellfun(@(x) ~isempty(x), regexp(levelNames, sprintf('%s$', level)));
 timeToSig = nan(numLevels, 1);
 fprintf('\nNeuron: %s\n', neuronName)
 for level_ind = 1:numLevels,
     fprintf('\t...%s\n', levelsOfInterest{level_ind})
     simTimeEst = mvnrnd(neuron.parEst(getTimeLevels(levelsOfInterest{level_ind})), stat.covb(getTimeLevels(levelsOfInterest{level_ind}), getTimeLevels(levelsOfInterest{level_ind})), numSim)';
-%     simMeanEst = mvnrnd(neuron.parEst(getMeanLevels(levelsOfInterest{level_ind})), stat.covb(getMeanLevel(levelsOfInterest{level_ind}), getMeanLevel(levelsOfInterest{level_ind})), numSim)';
+    simMeanEst = mvnrnd(neuron.parEst(getMeanLevel(levelsOfInterest{level_ind})), stat.covb(getMeanLevel(levelsOfInterest{level_ind}), getMeanLevel(levelsOfInterest{level_ind})), numSim)';
     timeEst = gam.bsplines{1}.unique_basis * gam.bsplines{1}.constraint * simTimeEst;
-%     timeEst = simMeanEst + timeEst;
+    timeEst = bsxfun(@plus, simMeanEst, timeEst);
     timeEstCI = quantile(timeEst, [0.025 0.975], 2);
     time = gam.bsplines{1}.x;
     
-    minTime = min(time(find(timeEstCI(:, 1) > 0, 1, 'first')), time(find(timeEstCI(:, 2) < 0, 1, 'first')));
+    lowerCIFirst = time(find(timeEstCI(:, 1) > 0, 1, 'first'));
+    upperCIFirst = time(find(timeEstCI(:, 2) < 0, 1, 'first'));
+    minTime = min([lowerCIFirst, upperCIFirst]);
     if ~isempty(minTime),
        timeToSig(level_ind) = minTime; 
     end
