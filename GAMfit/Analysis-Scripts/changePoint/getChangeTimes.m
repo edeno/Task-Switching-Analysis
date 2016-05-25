8c62d3bb5c0e13afed005bc91ef81ec530a62ab3
Eric Denovellis <edeno@bu.edu>
2016-05-25 16:22:46 -0400

    Consider only increases in derivative, find max increase time

--- GAMfit/Analysis-Scripts/changePoint/getChangeTimes.m
+++ GAMfit/Analysis-Scripts/changePoint/getChangeTimes.m
@@ -1,4 +1,4 @@
-function [sigChangeTimes, levelsOfInterest] = getChangeTimes(neuronName, covOfInterest, timePeriod, model, varargin)
+function [sigChangeTimes, maxChangeTimes, levelsOfInterest] = getChangeTimes(neuronName, covOfInterest, timePeriod, model, varargin)
 numSim = 1e4;
 pThresh = 1e-3;
 workingDir = getWorkingDir();
@@ -18,20 +18,20 @@ levelNames = [gam.levelNames];
 levelsOfInterest = covInfo(covOfInterest).levels;
 levelsOfInterest = levelsOfInterest(~ismember(levelsOfInterest, covInfo(covOfInterest).baselineLevel));
 numLevels = length(levelsOfInterest);
-
 getTimeLevels = @(level) cellfun(@(x) ~isempty(x), regexp(levelNames, sprintf('%s.Trial Time.*', level)));
 fprintf('\nNeuron: %s\n', neuronName)
 time = gam.bsplines{1}.x;
-time = time(51:end); % exclude first 50 ms because of potential edge effects
 sigChangeTimes = cell(numLevels, 1);
+maxChangeTimes = cell(numLevels, 1);
 for level_ind = 1:numLevels,
     fprintf('\t...%s\n', levelsOfInterest{level_ind})
     time_ind = getTimeLevels(levelsOfInterest{level_ind});
     simTimeEst = mvnrnd(neuron.parEst(time_ind), stat.covb(time_ind, time_ind), numSim)';
     timeEst = gam.bsplines{1}.unique_basis * gam.bsplines{1}.constraint * simTimeEst;
-    timeEst = timeEst(51:end, :); % exclude first 50 ms because of potential edge effects
     changeTimes = diff(timeEst, [], 1);
-    changeTimesCI = quantile(changeTimes, pThresh * [1 -1] + [0 1], 2);
-    sigChangeTimes{level_ind} = time(changeTimesCI(:, 1) > 0 | changeTimesCI(:, 2) < 0); % Only increases in firing rate
+    [~, max_ind] = max(changeTimes, [], 1);
+    maxChangeTimes{level_ind} = time(max_ind + 1);
+    changeTimesCI = quantile(changeTimes, pThresh, 2);
+    sigChangeTimes{level_ind} = time(changeTimesCI > 0) + 1;
 end
 end
