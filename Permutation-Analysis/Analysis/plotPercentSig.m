724676bac2c91cceb4f5531c5ed26e855f29291c
Eric Denovellis <edeno@bu.edu>
2016-06-13 15:13:24 -0400

    use color info

--- Permutation-Analysis/Analysis/plotPercentSig.m
+++ Permutation-Analysis/Analysis/plotPercentSig.m
@@ -1,20 +1,16 @@
 function plotPercentSig(comparisonsOfInterest)
 workingDir = getWorkingDir();
 analysisDir = sprintf('%s/Permutation-Analysis/Analysis', workingDir);
+load(sprintf('%s/paramSet.mat', workingDir), 'colorInfo');
+names = cellfun(@(x) strsplit(x, ' - '), comparisonsOfInterest, 'UniformOutput', false);
+names = cat(1, names{:});
+names = names(:, 1);
+colors = values(colorInfo, names);
 load(sprintf('%s/colllectedPermAnalysis.mat', analysisDir));
 brainAreas = {values.brainArea};
 timePeriods = values(1).timePeriod;
 uniqueBrainAreas = unique(brainAreas);
 uniqueTimePeriods = unique(timePeriods, 'stable')';
-
-colors = [27,158,119; ...
-    217,95,2; ...
-    117,112,179; ...
-    231,41,138; ...
-    102,166,30; ...
-    230,171,2; ...
-    166,118,29; ...
-    102,102,102] ./ 255;
 
 findBrainAreas = @(x) ismember(brainAreas, x);
 findComparison = @(x) ismember(comparisonNames, x);
@@ -33,13 +29,13 @@ figure;
 for area_ind = 1:length(uniqueBrainAreas),
     s{area_ind} = subplot(2,1,area_ind);
     p = plot(sig(:, :, area_ind), '.-', 'MarkerSize', 20);
-    set(p, {'Color'}, num2cell(colors(1:length(comparisonsOfInterest), :), 2));
+    set(p, {'Color'}, colors);
     box off;
     set(gca, 'XTick', 1:length(uniqueTimePeriods));
     set(gca, 'XTickLabel', uniqueTimePeriods);
     ylabel('Percent Signficant');
     t = text((0.1 + length(uniqueTimePeriods)) * ones(length(comparisonsOfInterest), 1), sig(end, :, area_ind), comparisonsOfInterest);
-    set(t, {'Color'}, num2cell(colors(1:length(comparisonsOfInterest), :), 2))
+    set(t, {'Color'},  colors)
     title(uniqueBrainAreas(area_ind));
     grid on;
 end
