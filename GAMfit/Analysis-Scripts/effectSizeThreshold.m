5cb32945b7614d5f272a58bc9ce3145fd8e7e486
edeno <edeno@bu.edu>
2016-06-17 11:21:47 -0400

    Update to use function

--- GAMfit/Analysis-Scripts/effectSizeThreshold.m
+++ GAMfit/Analysis-Scripts/effectSizeThreshold.m
@@ -1,52 +1,63 @@
-clear variables;
-brainArea = 'dlPFC';
-params.subject = '*';
-params.numSim = 1E4;
-modelName = 'Rule + Previous Error History + Rule Repetition';
-timePeriods = 'Rule Stimulus';
+load('paramSet.mat', 'colorInfo');
+numLevels = length(levelNames);
+numThresh = length(params.thresholds);
 
-[parEst, gam] = getCoef(modelName, timePeriods, 'brainArea', brainArea, 'isSim', true, 'subject', params.subject, 'numSim', params.numSim);
-
-bad_ind = abs(parEst) > 10;
-bad_ind(:, 1, :) = false;
-
-parEst(bad_ind) = NaN;
-
-parEst = 100 * (exp(parEst(:, 2:end, :)) - 1);
-
-thresholds = -100:1:100;
-thresholds(thresholds == 0) = [];
-percentGreaterThanThresh = nan(length(thresholds), size(parEst, 2), 3);
-
-for thresh_ind = 1:length(thresholds),
-    if thresholds(thresh_ind) > 0,
-        percentGreaterThanThresh(thresh_ind, :, :) = quantile(mean(double(parEst > thresholds(thresh_ind)), 1), [0.025, 0.5, 0.975], 3) * 100;
-    else
-        percentGreaterThanThresh(thresh_ind, :, :) = quantile(mean(double(parEst < thresholds(thresh_ind)), 1), [0.025, 0.5, 0.975], 3) * 100;
+for area_ind = 1:length(brainAreas),
+    areaData = cat(3, bootPercentGreaterThanThresh{area_ind, :});
+    f = figure;
+    f.Name = brainAreas{area_ind};
+    sub = numSubplots(numLevels);
+    for level_ind = 1:numLevels,
+        subplot(sub(1), sub(2), level_ind)
+        data = squeeze(areaData(level_ind, :, :));
+        plot(params.thresholds, data(2, :),  '.', 'Color', colorInfo(levelNames{level_ind}), 'MarkerSize', 10);
+        hold all;
+%         plot(params.thresholds, data(2, :),  '-', 'Color', colorInfo(levelNames{ind}), 'LineWidth', 2);
+        l = line(repmat(params.thresholds, [2, 1]), data([1 3], :));
+        set(l, 'Color', colorInfo(levelNames{level_ind}));
+        set(l, 'LineWidth', 1);
+        title(levelNames{level_ind});
+        ylim([0 100]);
+        box off;
+        xlabel('Percent Change in Firing Rate Threshold');
+        ylabel('Percentage of Neurons > Threshold');
     end
 end
 
-covNames = gam.covNames(2:end);
-load('paramSet.mat', 'colorInfo');
-levelNames = gam.levelNames(2:end);
-numLevels = length(levelNames);
 %%
-sub = numSubplots(numLevels);
-figure;
-for ind = 1:numLevels,
-    subplot(sub(1), sub(2), ind)
-    data = squeeze(percentGreaterThanThresh(:, ind, :));
-    plot(thresholds, data(:, 2),  '.', 'Color', colorInfo(levelNames{ind}), 'MarkerSize', 10);
-    hold all;
-    plot(thresholds, data(:, 2),  '-', 'Color', colorInfo(levelNames{ind}), 'LineWidth', 2);
-    l = line(repmat(thresholds, [2, 1]), data(:, [1 3])');
-    set(l, 'Color', colorInfo(levelNames{ind}));
-    set(l, 'LineWidth', 2);
-    title(levelNames{ind});
-    ylim([0 100]);
-    box off;
-    xlabel('Percent Change in Firing Rate Threshold');
-    ylabel('Percentage of Neurons > Threshold');
+load('paramSet.mat', 'colorInfo');
+numLevels = length(levelNames);
+numThresh = length(params.thresholds);
+
+flipNeg_ind = [(numThresh / 2):-1:1, (numThresh / 2)+1:numThresh];
+flipNeg_thresholds = [params.thresholds((numThresh / 2)+1:numThresh), params.thresholds((numThresh / 2)+1:numThresh)];
+
+for area_ind = 1:length(brainAreas),
+    areaData = cat(3, bootPercentGreaterThanThresh{area_ind, :});
+    f = figure;
+    f.Name = brainAreas{area_ind};
+    sub = numSubplots(numLevels);
+    for level_ind = 1:numLevels,
+        subplot(sub(1), sub(2), level_ind)
+        data = squeeze(areaData(level_ind, :, :));
+        l = line(repmat(flipNeg_thresholds, [2, 1]), data([1 3], flipNeg_ind));
+        set(l, 'Color', colorInfo(levelNames{level_ind}));
+        set(l, 'LineWidth', 0.5);
+        hold all;
+        plot(flipNeg_thresholds(1:(numThresh / 2)), data(2, flipNeg_ind(1:(numThresh / 2))),  '--', ...
+            'Color', colorInfo(levelNames{level_ind}), 'lineWidth', 3);
+        
+        plot(flipNeg_thresholds((numThresh / 2)+1:end), data(2, flipNeg_ind((numThresh / 2)+1:end)),  '-', ...
+            'Color', colorInfo(levelNames{level_ind}), 'lineWidth', 3);
+        posNeg_sig = comparePosNeg(:, level_ind, 1, area_ind) > 0 | comparePosNeg(:, level_ind, 3, area_ind) < 0;
+        posNeg_sig = double(posNeg_sig)';
+        posNeg_sig(posNeg_sig == 0) = NaN;
+        plot(flipNeg_thresholds((numThresh / 2)+1:end), 10 + (data(2, flipNeg_ind((numThresh / 2)+1:end)) .* posNeg_sig) ,  '*', ...
+            'Color', 'black', 'markerSize', 2.5);
+        title(levelNames{level_ind});
+        ylim([0 100]);
+        box off;
+        xlabel('Percent Change in Firing Rate Threshold');
+        ylabel('Percentage of Neurons > Threshold');
+    end
 end
-
-
